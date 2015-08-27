fb446604d16dd63cfbc2395f24524f8fd0ec2191
Paolo Patruno <p.patruno@iperbole.bologna.it>
2015-08-27 15:09:35 +0200

    work inside virtualenv

--- python/setup.py
+++ python/setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
-import os
+import os,errno,sys
 
 from distutils.command.build import build as build_
 from setuptools.command.develop import develop as develop_
@@ -8,12 +8,14 @@ from distutils.core import Command
 #from buildutils.cmd import Command
 #from distutils.cmd import Command
 
-from django.core import management
-from rmap import __version__
+try:
+    from rmap import __version__
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'rmap.settings'
-from django.conf import settings
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'rmap.settings'
+    from django.conf import settings
 
+except:
+    print "error setting django env"
 
 class distclean(Command):
     description = "remove man pages and *.mo files"
@@ -55,6 +57,7 @@ class build(build_):
 
 class  install(install_):
      def  run(self):
+         from django.core import management
          #print "eseguo compilemessages"
          management.call_command("compilemessages")
          #createmanpages().run()
@@ -73,6 +76,7 @@ class compilemessages(Command):
         pass
 
     def run(self):
+        from django.core import management
         management.call_command("compilemessages")
 
 class createmanpages(Command):
@@ -162,6 +166,14 @@ for dirpath, dirnames, filenames in os.walk('templates'):
     if filenames:
         data_files.append(['share/rmap/'+dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
+for dirpath, dirnames, filenames in os.walk('geoid_heights'):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if filenames:
+        data_files.append(['share/rmap/'+dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+
 #for dirpath, dirnames, filenames in os.walk('tables'):
 #    # Ignore dirnames that start with '.'
 #    for i, dirname in enumerate(dirnames):
@@ -170,9 +182,22 @@ for dirpath, dirnames, filenames in os.walk('templates'):
 #        data_files.append(['share/rmap/'+dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
 
-data_files.append(('/etc/rmap',['rmap-site.cfg']))
-data_files.append(('/etc/rmap',['map']))
+try:
+    if (os.path.isdir("/etc/rmap/")):
+        os.mkdir('/etc/rmap/tmp')
+        os.rmdir('/etc/rmap/tmp')
+    else:
+        os.mkdir('/etc/rmap')
+        os.rmdir('/etc/rmap')
 
+    data_files.append(('/etc/rmap',['rmap-site.cfg']))
+    data_files.append(('/etc/rmap',['map']))
+
+except OSError as e:
+    if (e[0] == errno.EACCES):
+       print >> sys.stderr, "You do not have root permissions to install files in /etc !"
+    else:
+        print >> sys.stderr, "There are some problems to install files in /etc !"
 
 #for dirpath, dirnames, filenames in os.walk('rmap/templates'):
 #    # Ignore dirnames that start with '.'
@@ -193,8 +218,6 @@ data_files.append(('/etc/rmap',['map']))
 #package_data.append('rmap_config')
 #package_data.append('settings')
 
-print ">>>>> locale:",data_files
-
 setup(name='rmap',
       version=__version__,
       description='rete monitoraggio ambientale partecipativo',
@@ -203,13 +226,14 @@ setup(name='rmap',
       platforms = ["any"],
       url='https://github.com/r-map/rmap',
       cmdclass={'build': build,'compilemessages':compilemessages,'createmanpages':createmanpages,"distclean":distclean,'install': install},
-      packages=['rmap','rmap.stations','rmap.stations.migrations','rmap.doc','mapview'],
+      packages=['rmap','rmap.stations','rmap.stations.migrations','rmap.doc','mapview','registration','registration.management','registration.backends','registration.backends.default','registration.backends.simple','registration.management.commands','paho','paho.mqtt'],
       package_data={'rmap': ['icons/*.png','tables/*.txt'],'rmap.stations': ['fixtures/*.json'],'mapview': ['icons/*.png'],},
-      scripts=['stationd','mqtt2graphited','borinudd','mqtt2dballed','poweroffd','composereportd','rmapweb','amqp2amqp_identvalidationd','amqp2dballed', 'amqp2arkimetd','amqp2mqttd','rmap-configure','rmapctrl','rmap.wsgi','main.py'],
+      scripts=['stationd','mqtt2graphited','borinudd','mqtt2dballed','poweroffd','composereportd','rmapweb','amqp2amqp_identvalidationd','amqp2dballed', 'amqp2arkimetd','amqp2mqttd','rmap-configure','rmapctrl','rmap.wsgi','rmapgui'],
       data_files = data_files,
       license = "GNU GPL v2",
-      install_requires= [ "django","kivy","pika","configobj"],
-      setup_requires= [ "django","configobj"],
+      install_requires= [ "django","configobj","kivy","plyer","pika","simplejson","futures"],
+      #install_requires= [ "django","Cython","pil","pysdl2","kivy","plyer","configobj","pika","simplejson"],
+      #setup_requires= [ "django","configobj"],
       long_description="""\
 R-map: participative environmental monitoring net.
 """
