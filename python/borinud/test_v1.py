f6e0552f7fe01fbb38f2780bdf6dba87108a3f16
Emanuele Di Giacomo <edigiacomo@arpa.emr.it>
2015-11-07 09:01:44 +0100

    borinud test suite for v1 API

---
+++ python/borinud/test_v1.py
@@ -0,0 +1,66 @@
+import os
+import tempfile
+from datetime import datetime
+
+from django.test import TestCase
+from django.test import Client
+
+import dballe
+
+from .models import Source, Summary
+from .utils import sync_source
+
+
+class TestUtils(TestCase):
+    def setUp(self):
+        self.dbfile = tempfile.NamedTemporaryFile(
+            mode="w", delete=False, suffix=".sqlite3",
+        ).name
+        self.db = dballe.DB.connect_from_file(self.dbfile)
+        self.db.reset()
+        for r in [{
+            "ident": None,
+            "rep_memo": "test_net",
+            "lon": 1200000,
+            "lat": 4300000,
+            "datetime": datetime(2015, 1, 1),
+            "B12101": 0,
+            "trange": (254, 0, 0),
+            "level": (103, 2000),
+        }, {
+            "ident": None,
+            "rep_memo": "test_net",
+            "lon": 1200000,
+            "lat": 4300000,
+            "datetime": datetime(2015, 1, 2),
+            "B12101": 0,
+            "trange": (254, 0, 0),
+            "level": (103, 2000),
+        }, {
+            "ident": "test_sta",
+            "rep_memo": "test_net",
+            "lon": 1200000,
+            "lat": 4300000,
+            "datetime": datetime(2015, 1, 1),
+            "B12101": 0,
+            "trange": (254, 0, 0),
+            "level": (103, 2000),
+        }, {
+            "ident": "test_sta",
+            "rep_memo": "test_net",
+            "lon": 1300000,
+            "lat": 4200000,
+            "datetime": datetime(2015, 1, 2),
+            "B12101": 0,
+            "trange": (254, 0, 0),
+            "level": (103, 2000),
+        }]:
+            self.db.insert_data(dballe.Record(**r), can_add_stations=True)
+
+
+    def tearDown(self):
+        os.remove(self.dbfile)
+
+    def test_summaries(self):
+        c = Client()
+        response = c.get("/borinud/api/v1/*/*/*/*/*/*/summaries")
