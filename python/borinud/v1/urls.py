e54f59e4ebfa1ae8bd01e58ddba562374ae1c37b
Emanuele Di Giacomo <edigiacomo@arpa.emr.it>
2015-11-09 11:08:20 +0100

    borinud v1 generic summary url

--- python/borinud/v1/urls.py
+++ python/borinud/v1/urls.py
@@ -4,6 +4,13 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^\*/\*/\*/\*/\*/\*/summaries$', views.summaries),
-    url(r'^\*/\*/(?P<network>\w+)/\*/\*/\*/summaries$', views.summaries),
+    url((
+        r'^(?P<ident>\w+|\*)'
+        r'/(?P<coords>(?P<lon>\d+),(?P<lat>\d+)|\*)'
+        r'/(?P<network>\w+|\*)'
+        r'/(?P<trange>(?P<tr>\d+|-),(?P<p1>\d+|-),(?P<p2>\d+|-)|\*)'
+        r'/(?P<level>(?P<lt1>\d+|-),(?P<lv1>\d+|-),(?P<lt2>\d+|-),(?P<lv2>\d+|-)|\*)'
+        r'/(?P<var>B\d{5}|\*)'
+        r'/summaries'
+    ), views.summaries),
 ]
--- python/borinud/v1/utils.py
+++ python/borinud/v1/utils.py
@@ -3,9 +3,25 @@
 # Author: Emanuele Di Giacomo <emanueledigiacomo@gmail.com>
 
 def path2query(d):
-    return {
+    q = {
         k: d[k] if d[k] != "-" else None
         for k in ["ident", "lon", "lat", "network",
                   "var", "tr", "p1", "p2", "lt1", "lv1", "lt2", "lv2"]
         if k in d and d[k] != "*"
     }
+    if "coords" in d and d["coords"] == "*":
+        del q["lon"]
+        del q["lat"]
+
+    if "trange" in d and d["trange"] == "*":
+        del q["tr"]
+        del q["p1"]
+        del q["p2"]
+
+    if "level" in d and d["level"] == "*":
+        del q["lt1"]
+        del q["lv1"]
+        del q["lt2"]
+        del q["lv2"]
+
+    return q
