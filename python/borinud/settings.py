0c8f6761a1332bff638c774bc4326ebaf86cefde
Emanuele Di Giacomo <edigiacomo@arpa.emr.it>
2016-01-07 12:52:05 +0100

    Comments for borinud settings

--- python/borinud/settings.py
+++ python/borinud/settings.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 
 DEFAULTS = {
+    # List of databases (e.g. ["sqlite:/path/to/db.sqlite3"]).
     "SOURCES": [],
+    # Name of the cache used for the summary (None if disabled)
     "CACHED_SUMMARY": None,
+    # Cache timeout
     "CACHED_SUMMARY_TIMEOUT": 0,
 }
 
