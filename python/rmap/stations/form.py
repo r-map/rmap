caef1ee21bedf7d01e6e93e38a60d1498bfa30b2
Paolo Patruno <ppatruno@arpa.emr.it>
2016-01-27 17:43:12 +0100

    close #87

--- python/rmap/stations/form.py
+++ python/rmap/stations/form.py
@@ -12,8 +12,8 @@ class RmapRegistrationForm(RegistrationFormTermsOfService):
     """
     Custom form to limit user length and content
     """
-    username = forms.CharField(label='Username',help_text=_('Required: max 9 alphanumeric characters'), max_length=9,
+    username = forms.CharField(label='Username',help_text=_('Required: max 9 lowercase alphanumeric characters'), max_length=9,
                                validators=[
-                                   validators.RegexValidator(re.compile('^[\w]+$'), _('Enter a valid username.'), 'invalid')
+                                   validators.RegexValidator(re.compile('^[a-z0-9_]+$'), _('Enter a valid username.'), 'invalid')
                                ])
 
