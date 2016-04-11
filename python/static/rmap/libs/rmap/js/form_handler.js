ac2e476998bbe6c442c29d9aa47b6297bd0e7f94
Paolo Patruno <p.patruno@iperbole.bologna.it>
2016-04-11 09:41:55 +0200

    rmap release 6.12 with new web look with bootstrap

---
+++ python/static/rmap/libs/rmap/js/form_handler.js
@@ -0,0 +1,6 @@
+$("form").addClass("form-inline");
+$("textarea").addClass("form-control");
+$("select").addClass("form-control");
+$("input").addClass("form-control");
+$(".helptext").addClass("help-block");
+$(".errorlist").addClass("alert alert-danger");
