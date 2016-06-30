47a1497e4474c34da31c6d9997a20d6adb8c4b78
Emanuele Di Giacomo <emanuele.digiacomo@gmail.com>
2016-06-30 07:59:35 +0200

    Borinud: hyphen allowed in network url

--- python/borinud/v1/urls.py
+++ python/borinud/v1/urls.py
@@ -8,7 +8,7 @@ from . import views
 basepattern = (
     r'^(?P<ident>\w+|\*|-)'
     r'/(?P<coords>(?P<lon>\d+|-),(?P<lat>\d+|-)|\*)'
-    r'/(?P<network>\w+|\*)'
+    r'/(?P<network>[-\w]+|\*)'
     r'/(?P<trange>(?P<tr>\d+|-|\*),(?P<p1>\d+|-|\*),(?P<p2>\d+|-|\*)|\*)'
     r'/(?P<level>(?P<lt1>\d+|-|\*),(?P<lv1>\d+|-|\*),(?P<lt2>\d+|-|\*),(?P<lv2>\d+|-|\*)|\*)'
     r'/(?P<var>B\d{5}|\*)'
