3390c49a132494c9fbf39d15c87043921c07dcda
Paolo Patruno <p.patruno@iperbole.bologna.it>
2016-09-16 13:42:16 +0200

    confguration for i2c-th

---
+++ arduino/sketchbook/rmap/sensor_config/registers-th.h
@@ -0,0 +1,1 @@
+../i2c-th/registers-th.h
--- arduino/sketchbook/rmap/sensor_config/sensor_config.ino
+++ arduino/sketchbook/rmap/sensor_config/sensor_config.ino
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "Wire.h"
 #include <Arduino.h>
 #include "registers-wind.h"         //Register definitions
+#include "registers-th.h"         //Register definitions
 
 const char version[] = "1.0";
 
@@ -41,6 +42,7 @@ void displayHelp()
   Serial.println();
   Serial.println(F("Sensor to config:"));
   Serial.println(F("\tw = i2c-wind"));
+  Serial.println(F("\tt = i2c-th"));
   Serial.println(F("\th = hih humidity sensorr"));
   //Serial.println(F("Output:"));
   //Serial.println(F("\tp = toggle printAll - printFound."));
@@ -74,15 +76,16 @@ void setup() {
 
 void loop() {
 
-  int new_address= -1;
-  int oneshot= -1;
-  int sensortype =- 1;
+  int new_address;
+  int oneshot;
+  int sensortype;
 
   char command = getCommand();
   switch (command)
     {
     case 'w':
       
+      new_address= -1;
       while (new_address < 1 || new_address > 127){
 	Serial.println(F("digit new i2c address for i2c-wind (1-127)"));
 	new_address=Serial.parseInt();
@@ -97,6 +100,7 @@ void loop() {
       
       delay(1000);
 
+      sensortype=-1;
       while (sensortype < 1 || sensortype > 2){
 	Serial.println(F("digit sensortype code for i2c-wind (1 Davis, 2 Inspeed)"));
 	sensortype=Serial.parseInt();
@@ -111,6 +115,7 @@ void loop() {
       
       delay(1000);
 
+      oneshot=-1;
       while (oneshot < 0 || oneshot > 1){
 	Serial.println(F("digit 1 for oneshotmode; 0 for continous mode for i2c-wind (0/1)"));
 	oneshot=Serial.parseInt();
@@ -132,6 +137,79 @@ void loop() {
       Serial.println(F("Done; switch off"));
       delay(10000);
 
+
+      displayHelp();
+      break;
+
+    case 't':
+
+      new_address=-1;
+      while (new_address < 1 || new_address > 127){
+	Serial.println(F("digit new i2c address for i2c-th (1-127)"));
+	new_address=Serial.parseInt();
+	Serial.println(new_address);
+      }
+
+      delay(1000);      
+
+      Wire.beginTransmission(I2C_TH_DEFAULTADDRESS);
+      Wire.write(I2C_TH_ADDRESS);
+      Wire.write(new_address);
+      if (Wire.endTransmission() != 0) Serial.println(F("Wire Error"));             // End Write Transmission 
+      
+      delay(1000);
+
+      new_address=-1;
+      while (new_address < 1 || new_address > 127){
+	Serial.println(F("digit new i2c_temperature address for i2c-th (1-127)"));
+	new_address=Serial.parseInt();
+	Serial.println(new_address);
+      }
+      delay(1000);
+      
+      Wire.beginTransmission(I2C_TH_DEFAULTADDRESS);
+      Wire.write(I2C_TH_TEMPERATUREADDRESS);
+      Wire.write(new_address);
+      if (Wire.endTransmission() != 0) Serial.println(F("Wire Error"));             // End Write Transmission 
+      
+      delay(1000);
+
+      new_address=-1;
+      while (new_address < 1 || new_address > 127){
+	Serial.println(F("digit new i2c_humidity address for i2c-th (1-127)"));
+	new_address=Serial.parseInt();
+	Serial.println(new_address);
+      }
+      delay(1000);
+      
+      Wire.beginTransmission(I2C_TH_DEFAULTADDRESS);
+      Wire.write(I2C_TH_HUMIDITYADDRESS);
+      Wire.write(new_address);
+      if (Wire.endTransmission() != 0) Serial.println(F("Wire Error"));             // End Write Transmission 
+      
+      delay(1000);
+
+      oneshot=-1;
+      while (oneshot < 0 || oneshot > 1){
+	Serial.println(F("digit 1 for oneshotmode; 0 for continous mode for i2c-th (0/1)"));
+	oneshot=Serial.parseInt();
+	Serial.println(oneshot);
+      }
+      delay(1000);
+      
+      Wire.beginTransmission(I2C_TH_DEFAULTADDRESS);
+      Wire.write(I2C_TH_ONESHOT);
+      Wire.write((bool)oneshot);
+      if (Wire.endTransmission() != 0) Serial.println(F("Wire Error"));             // End Write Transmission 
+      
+      delay(1000);
+      Wire.beginTransmission(I2C_TH_DEFAULTADDRESS);
+      Wire.write(I2C_TH_COMMAND);
+      Wire.write(I2C_TH_COMMAND_SAVE);
+      if (Wire.endTransmission() != 0)  Serial.println(F("Wire Error"));             // End Write Transmission 
+      
+      Serial.println(F("Done; switch off"));
+      delay(10000);
 
       displayHelp();
       break;
