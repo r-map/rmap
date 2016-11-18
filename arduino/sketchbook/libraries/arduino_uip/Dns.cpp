0cdc9da2abda1535d2a50ec44cff9c9165383b2d
Paolo Patruno <p.patruno@iperbole.bologna.it>
2016-11-18 19:42:49 +0100

    solved timeout in name resolver

--- arduino/sketchbook/libraries/arduino_uip/Dns.cpp
+++ arduino/sketchbook/libraries/arduino_uip/Dns.cpp
@@ -134,10 +134,10 @@ int DNSClient::getHostByName(const char* aHostname, IPAddress& aResult)
     // Find a socket to use
     if (iUdp.begin(1024+(millis() & 0xF)) == 1)
     {
-        // Try up to three times
-        int retries = 0;
+//        // Try up to three times
+//        int retries = 0;
 //        while ((retries < 3) && (ret <= 0))
-        {
+//        {
             // Send DNS request
             ret = iUdp.beginPacket(iDNSServer, DNS_PORT);
             if (ret != 0)
@@ -151,18 +151,18 @@ int DNSClient::getHostByName(const char* aHostname, IPAddress& aResult)
                     if (ret != 0)
                     {
                         // Now wait for a response
-                        int wait_retries = 0;
-                        ret = TIMED_OUT;
-                        while ((wait_retries < 3) && (ret == TIMED_OUT))
-                        {
+//                        int wait_retries = 0;
+//                      ret = TIMED_OUT;
+//                        while ((wait_retries < 1) && (ret == TIMED_OUT))
+//                        {
                             ret = ProcessResponse(5000, aResult);
-                            wait_retries++;
-                        }
+//                            wait_retries++;
+//                        }
                     }
                 }
             }
-            retries++;
-        }
+//            retries++;
+//        }
 
         // We're done with the socket now
         iUdp.stop();
