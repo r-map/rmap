5e0c987fbf162636221d36182792b960f734cf90
Paolo Patruno <p.patruno@iperbole.bologna.it>
2016-12-13 01:26:51 +0100

    bug in geojson api v1; added showdata django application

---
+++ python/showdata/views.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+# Author: Paolo Patruno <p.patruno@iperbole.bologna.it>
+
+from django.shortcuts import render
+
+def spatialseries(request, **kwargs):
+
+    return render(request, 'showdata/spatialseries.html',{"ident":kwargs.get("ident"), "coords":kwargs.get("coords"), "network":kwargs.get("network"), "trange":kwargs.get("trange"), "level":kwargs.get("level"), "var":kwargs.get("var"), "year":kwargs.get("year"), "month":kwargs.get("month"), "day":kwargs.get("day")})
+
+
+def stationdata(request, **kwargs):
+
+    return render(request, 'showdata/stationdata.html',{"ident":kwargs.get("ident"), "coords":kwargs.get("coords"), "network":kwargs.get("network"), "trange":kwargs.get("trange"), "level":kwargs.get("level"), "var":kwargs.get("var")})
+
