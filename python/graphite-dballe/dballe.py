15ea1a88e26174fbc4e732a48d0f72e03e0e825a
Paolo Patruno <ppatruno@arpa.emr.it>
2017-01-04 19:58:07 +0100

    working on dballe interface for graphite

--- python/graphite-dballe/dballe.py
+++ python/graphite-dballe/dballe.py
@@ -1,69 +1,122 @@
 # documentation at http://graphite.readthedocs.io/en/latest/storage-backends.html
 
 import time
-#import requests
+import requests
 import re
+import dateutil.parser
 
 from .intervals import Interval, IntervalSet
 from .node import LeafNode, BranchNode
 from rmap.settings import *
+from itertools import groupby
 
-class DballeFinder(object):
+def sortandgroup(rj,key):
+    return groupby(sorted(rj, key=lambda staz: staz[key]),key=lambda staz: staz[key])
 
-#    def is_branch(self,path):
-#        return not "@" in path
 
-    def find_nodes(self, query):
-        # find some paths matching the query, then yield them
+def path2uri(path):
+    #uri="/*/*/*/*/*/*"
 
-        print "query: ", query
+    #print "path=",path
+    uri = path.replace(".","/").replace("_",",")
+    uri+="/*" * (5-uri.count("/"))
+    return uri
 
-        #r=requests.get("http://rmap.cc/borinud/api/v2/"+query2path(query)+"/summary")
-        #r.json()
+class  wssummaries(object):
 
-        #query:  <FindQuery: due/duedue/dueduedue.* from * until *>
+    def __init__(self,query):
+        self.query=query
+
+    def __iter__(self):
+
         #query.pattern
         #query.startTime
         #query.endTime
 
-        metad=BORINUD ["SOURCES"][0]["measurements"] 
+        self.branch =True
 
-        p = re.compile(query.pattern.replace(".","\.").replace("*",".*"))
+        position=self.query.pattern.count(".")
+        if position == 0:
+            self.key="ident"
+        elif position == 1:
+            self.key="lonlat"
+        elif position == 2:
+            self.key="network"
+        elif position == 3:
+            self.key="timerange"
+        elif position == 4:
+            self.key="level"
+        elif position == 5:
+            self.key="var"
+            self.branch=False
+        else:
+            raise "error in graphite query to dballe"
 
-        # if query.pattern == "*":
-        #     matches=["uno","due","tre"]
-        # elif query.pattern == "uno.*":
-        #     matches=["menouno","menodue","menotre"]
-        # elif query.pattern == "due.*":
-        #     matches=["ugualeuno","ugualedue","ugualetre"]
-        # elif query.pattern == "tre.*":
-        #     matches=["piuuno","piudue","piutre"]
-        # else:
-        #     matches=["misura1@","misura2@","misura3@"]
+        #p = re.compile(query.pattern.replace(".","\.").replace("*",".*"))
+        uri=path2uri(self.query.pattern)
+        
+        r=requests.get("http://127.0.0.1:8888/borinud/api/v1/dbajson/"+uri+"/summaries")
+        rj=r.json()
 
-        for mymeta in metad:
+        self.summaries=[]
+        for station in rj:
+            newstation={}
+            newstation["ident"]=station["ident"]                
+            newstation["lonlat"]=str(station["lon"])+"_"+str(station["lat"])
+            newstation["network"]=station["network"]
 
-            trange = "%s_%s_%s" % tuple(("-" if v is None else str(v) for v in mymeta["trange"]))
-            level  = "%s_%s_%s_%s" % tuple(("-" if v is None else str(v) for v in mymeta["level"]))
-            var=mymeta["var"]
-            path=trange+"."+level+"."+var
+            for data in station["data"]:
+                #print "data: ",data
 
-            position=query.pattern.count(".")
-            if position == 0:
-                node=trange
-            elif position == 1:
-                node=level
-            elif position == 2:
-                node=var
+                for i,value in enumerate(data["level"]):
+                    if data["level"][i] is None:
+                        data["level"][i]="-"
+                    else:
+                        data["level"][i]=str(data["level"][i])
+
+                for i,value in enumerate(data["timerange"]):
+                    if data["timerange"][i] is None:
+                        data["timerange"][i]="-"
+                    else:
+                        data["timerange"][i]=str(data["timerange"][i])
+                            
+                newstation["level"]= data["level"][0]+"_"+data["level"][1]+"_"+data["level"][2]+"_"+data["level"][3]
+                newstation["timerange"]=data["timerange"][0]+"_"+data["timerange"][1]+"_"+data["timerange"][2]
+
+                for key in data["vars"].keys():
+                    newstation["var"]=key
+                            
+                    self.summaries.append(newstation)
+
+                
+        return self.next()
+
+    def next(self):
+        
+        for k,g in sortandgroup(self.summaries,self.key):
+            #print "---->>",self.key," = ",k
+            #for station in g:
+            #    print "---->> station = ",station
+        
+            if k is None:
+                k="-"
+                
+            self.node=str(k)
+
+            yield self.branch,self.node
+
+
+class DballeFinder(object):
+
+    def find_nodes(self, query):
+        # find some paths matching the query, then yield them
+        for branch,node in wssummaries(query):
+
+            if branch:
+                yield BranchNode(node)
+            else:
+                yield LeafNode(node, DballeReader(query.pattern))
             
-            if  not (p.match(path) is None):
-                #if   query.pattern[:-2].endswith(var):
-                if   node == var:
-                    yield LeafNode(path, DballeReader(node))
-                else:
-                    yield BranchNode(node)
-
-
 #LeafNode is created with a reader, which is the class responsible for
 #fetching the datapoints for the given path. It is a simple class with
 #2 methods: fetch() and get_intervals():
@@ -86,144 +139,69 @@ class DballeReader(object):
 
     def __init__(self, path):
         self.path = path
-
+        print "path: ", self.path
+        
     def fetch(self, start_time, end_time):
-        # fetch data
 
+        # fetch data
         print "fetch: ",start_time,end_time
+        
+        uri=path2uri(self.path)
+        
+        #check which query we have to do
+        mytime=time.gmtime(start_time)
+        print "mytime: ",mytime
+
+        #hour
+        #r=requests.get("http://127.0.0.1:8888/borinud/api/v1/dbajson/"+uri+"/timeseries/"+"{:04d}".format(mytime.tm_year)+"/{:02d}".format(mytime.tm_mon)+"/{:02d}".format(mytime.tm_mday)+"/{:02d}".format(mytime.tm_hour))
+        #day
+        r=requests.get("http://127.0.0.1:8888/borinud/api/v1/dbajson/"+uri+"/timeseries/"+"{:04d}".format(mytime.tm_year)+"/{:02d}".format(mytime.tm_mon)+"/{:02d}".format(mytime.tm_mday))
+        #month
+        #r=requests.get("http://127.0.0.1:8888/borinud/api/v1/dbajson/"+uri+"/timeseries/"+"{:04d}".format(mytime.tm_year)+"/{:02d}".format(mytime.tm_mon))
+
+        rj=r.json()
+
+        series=[]
+        for station in rj:
+            print "station: ", station
+            val=station["data"][0]["vars"][uri.split("/")[-1]]["v"]
+
+            # todo: put data in an equaly tyme spaced array
+            
+            print "val: ",val
+            series.append(val)
+        print series
+
+        if len(series) > 0:
+            step=int((int(end_time)-int(start_time))/len(rj))
+            time_info=(int(start_time), int(end_time),step)
+        else:
+            time_info=(int(start_time), int(end_time),end_time-start_time)
+
+        print "step: ",step
+        print "time_info: ",time_info
+        
         #time_info = _from_, _to_, _step_
-        time_info=(int(time.time()-100), int(time.time()),1)
-        series=range(*time_info)
+        #time_info=(int(time.time()-100), int(time.time()),1)
+        #series=range(*time_info)
         return time_info, series
 
     def get_intervals(self):
-        print "getintervals"
+        #print "getintervals"
         #return IntervalSet([Interval(start, end)])
+
+        uri=path2uri(self.path)
+        r=requests.get("http://127.0.0.1:8888/borinud/api/v1/dbajson/"+uri+"/summaries")
+        rj=r.json()
+
+        start=rj[0]["date"][0]
+        end=rj[0]["date"][1]
+
+        startdate = dateutil.parser.parse(start)  
+        enddate   = dateutil.parser.parse(end)
+
+        #return IntervalSet([Interval(int(time.mktime(startdate.timetuple())),int(time.mktime(enddate.timetuple())))])
+        
         return IntervalSet([Interval(int(time.time())-100, int(time.time()))])
 
 
-
-#STORAGE_FINDERS = (
-#    'graphite-dballe.DballeFinder',
-#)
-
-
-
-# # Cyanite example
-
-# import requests
-
-
-# def chunk(nodelist, length):
-#     chunklist = []
-#     linelength = 0
-#     for node in nodelist:
-#         # the magic number 6 is because the nodes list gets padded
-#         # with '&path=' in the resulting request
-#         nodelength = len(str(node)) + 6
-
-#         if linelength + nodelength > length:
-#             yield chunklist
-#             chunklist = [node]
-#             linelength = nodelength
-#         else:
-#             chunklist.append(node)
-#             linelength += nodelength
-#     yield chunklist
-
-
-# class CyaniteLeafNode(LeafNode):
-#     __fetch_multi__ = 'cyanite'
-
-
-# class URLs(object):
-#     def __init__(self, hosts):
-#         self.iterator = itertools.cycle(hosts)
-
-#     @property
-#     def host(self):
-#         return next(self.iterator)
-
-#     @property
-#     def paths(self):
-#         return '{0}/paths'.format(self.host)
-
-#     @property
-#     def metrics(self):
-#         return '{0}/metrics'.format(self.host)
-# urls = None
-# urllength = 8000
-
-
-# class CyaniteReader(object):
-#     __slots__ = ('path',)
-
-#     def __init__(self, path):
-#         self.path = path
-
-#     def fetch(self, start_time, end_time):
-#         data = requests.get(urls.metrics, params={'path': self.path,
-#                                                   'from': start_time,
-#                                                   'to': end_time}).json()
-#         if 'error' in data:
-#             return (start_time, end_time, end_time - start_time), []
-#         if len(data['series']) == 0:
-#             return
-#         time_info = data['from'], data['to'], data['step']
-#         return time_info, data['series'].get(self.path, [])
-
-#     def get_intervals(self):
-#         # TODO use cyanite info
-#         return IntervalSet([Interval(0, int(time.time()))])
-
-
-# class CyaniteFinder(object):
-#     __fetch_multi__ = 'cyanite'
-
-#     def __init__(self, config=None):
-#         global urls
-#         global urllength
-#         if config is not None:
-#             if 'urls' in config['cyanite']:
-#                 urls = config['cyanite']['urls']
-#             else:
-#                 urls = [config['cyanite']['url'].strip('/')]
-#             if 'urllength' in config['cyanite']:
-#                 urllength = config['cyanite']['urllength']
-#         else:
-#             from django.conf import settings
-#             urls = getattr(settings, 'CYANITE_URLS')
-#             if not urls:
-#                 urls = [settings.CYANITE_URL]
-#             urllength = getattr(settings, 'CYANITE_URL_LENGTH', urllength)
-#         urls = URLs(urls)
-
-#     def find_nodes(self, query):
-#         paths = requests.get(urls.paths,
-#                              params={'query': query.pattern}).json()
-#         for path in paths:
-#             if path['leaf']:
-#                 yield CyaniteLeafNode(path['path'],
-#                                       CyaniteReader(path['path']))
-#             else:
-#                 yield BranchNode(path['path'])
-
-#     def fetch_multi(self, nodes, start_time, end_time):
-
-#         paths = [node.path for node in nodes]
-#         data = {}
-#         for pathlist in chunk(paths, urllength):
-#             tmpdata = requests.get(urls.metrics,
-#                                    params={'path': pathlist,
-#                                            'from': start_time,
-#                                            'to': end_time}).json()
-#             if 'error' in tmpdata:
-#                 return (start_time, end_time, end_time - start_time), {}
-
-#             if 'series' in data:
-#                 data['series'].update(tmpdata['series'])
-#             else:
-#                 data = tmpdata
-
-#         time_info = data['from'], data['to'], data['step']
-#         return time_info, data['series']
