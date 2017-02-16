9926caeb159bd7ff23a11d5272e60a08a133250b
Marco Baldinetti <m.baldinetti@digiteco.it>
2017-02-16 16:17:56 +0100

    update sketch test_hyt271 and add SensorDriver library for HYT271

--- arduino/sketchbook/rmap/test_hyt271/test_hyt271.ino
+++ arduino/sketchbook/rmap/test_hyt271/test_hyt271.ino
@@ -10,19 +10,30 @@ void test_read_ht() {
 
   if (Wire.available() == I2C_HYT271_READ_HT_DATA_LENGTH) {
     HYT271_getHT((unsigned long) Wire.read() << 24 | (unsigned long) Wire.read() << 16 | (unsigned long) Wire.read() << 8 | (unsigned long) Wire.read(), &humidity, &temperature);    
-    Serial.print("Humidity: ");
+    Serial.print("Sensor HYT271 on 0x");
+    Serial.print(I2C_HYT271_DEFAULT_ADDRESS,HEX);
+    Serial.println("");
+    
+    Serial.print("---> Humidity: ");
     Serial.print(humidity);
-    Serial.print("% - Temperature: ");
+    Serial.print(" % \t\tB13003: ");
+    Serial.println(round(humidity));
+
+    Serial.print("---> Temperature: ");
     Serial.print(temperature);
+    Serial.print(" ");
     Serial.write(0xB0); // °
-    Serial.println("C");
+    Serial.print("C \tB12101: ");
+    Serial.print(temperature + 273.15);
+    Serial.println("");
+    Serial.println("");
   }
 }
 
 void setup() {
   Serial.begin(115200);
   Wire.begin();
-  Wire.setClock(400000L); // 400 KHz
+  Wire.setClock(50000L); // 400 KHz
 }
 
 void loop() {
