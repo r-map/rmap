2f28ce9265bd7856201eb3cae972ba6105bb2b82
Marco Baldinetti <m.baldinetti@digiteco.it>
2017-02-16 16:35:53 +0100

    fix hyt271.h

--- arduino/sketchbook/libraries/HYT271/hyt271.h
+++ arduino/sketchbook/libraries/HYT271/hyt271.h
@@ -6,10 +6,10 @@ extern "C"
 {
 #endif
 	
-	#define I2C_HYT271_DEFAULT_ADDRESS					(0x28)
-	#define I2C_HYT271_READ_HT_DATA_LENGTH			(4)
-	
-	unsigned char HYT271_getHT(unsigned long, float *, float *);
+#define I2C_HYT271_DEFAULT_ADDRESS				(0x28)
+#define I2C_HYT271_READ_HT_DATA_LENGTH				(4)
+
+unsigned char HYT271_getHT(unsigned long, float *, float *);
 	
 #ifdef __cplusplus
 }
