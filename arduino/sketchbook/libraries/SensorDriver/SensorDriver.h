090da4363a5feaa3f1b4b0a0c3c2fc2c438f1932
Alduss <lettieri.aldo@gmail.com>
2017-04-25 20:30:23 +0200

    Added class methods for definition Si7021

--- arduino/sketchbook/libraries/SensorDriver/SensorDriver.h
+++ arduino/sketchbook/libraries/SensorDriver/SensorDriver.h
@@ -585,6 +585,43 @@ class SensorDriverTmp : public SensorDriver
 
 #endif
 
+#if defined (HI7021DRIVER)
+
+#define SI7021_DEFAULT_ADDRESS         (0x40)
+
+#define SI7021_MEASRH_HOLD_CMD           0xE5
+#define SI7021_MEASRH_NOHOLD_CMD         0xF5
+#define SI7021_MEASTEMP_HOLD_CMD         0xE3
+#define SI7021_MEASTEMP_NOHOLD_CMD       0xF3
+#define SI7021_READPREVTEMP_CMD          0xE0
+#define SI7021_RESET_CMD                 0xFE
+#define SI7021_WRITERHT_REG_CMD          0xE6
+#define SI7021_READRHT_REG_CMD           0xE7
+#define SI7021_WRITEHEATER_REG_CMD       0x51
+#define SI7021_READHEATER_REG_CMD        0x11
+#define SI7021_ID1_CMD                   0xFA0F
+#define SI7021_ID2_CMD                   0xFCC9
+#define SI7021_FIRMVERS_CMD              0x84B8
+
+
+ class SensorDriverSI7021 : public SensorDriver
+ {
+ public:
+   virtual int setup(const char* driver, const int address, const int node, const char* type
+  #if defined (RADIORF24)
+		     , char* mainbuf, size_t lenbuf, RF24Network* network
+    #if defined (AES)
+		     , uint8_t key[] , uint8_t iv[]
+    #endif
+  #endif
+		     );
+    virtual int prepare(unsigned long& waittime);
+    virtual int get(long values[],size_t lenvalues);
+  #if defined(USEAJSON)
+    virtual aJsonObject* getJson();
+  #endif
+};
+#endif
 
 #define SD_INTERNAL_ERROR 0
 #define SD_SUCCESS 1
