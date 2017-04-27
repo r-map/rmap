d6fca4db8c9a997d4a98f8b3293843777bdd7f2b
pat1 <p.patruno@iperbole.bologna.it>
2017-04-27 19:45:46 +0200

    try to do not get error on exit (app)

--- python/rmap/rmapgui.py
+++ python/rmap/rmapgui.py
@@ -1379,11 +1379,11 @@ class Rmap(App):
 
 
 
-    def close_settings(self, settings):
+    def close_settings(self):
         """ The settings panel has been closed. """
         print "Setting was closed"
         self.questionactivatestation()
-        super(Rmap, self).close_settings(settings)
+        super(Rmap, self).close_settings()
 
 
     def stationstatus(self):
