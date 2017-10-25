885328972f38be94da441116c5d3389287b0040f
Emanuele Di Giacomo <edigiacomo@arpae.it>
2017-10-25 09:37:33 +0200

    Convert Arpae air quality data to BUFR

---
+++ arpae_aq_ckan_to_bufr/arpae_aq_ckan_to_bufr/__init__.py
@@ -0,0 +1,165 @@
+import csv
+import json
+import codecs
+from datetime import datetime
+import logging
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
+try:
+    from urllib.parse import urlsplit, parse_qsl, urlencode, urlunsplit
+except ImportError:
+    from urllib import urlencode
+    from urlparse import urlsplit, urlunsplit, parse_qsl
+
+import dballe
+
+
+logger = logging.getLogger(__name__)
+
+# Devo convertire gli id in bcode. Alcuni mancano o non sono in grado io di
+# stabilire una corrispondenza...
+VARIABLE_BCODES = {
+    1: "B15197",
+    5: "B15195",
+    7: "B15194",
+    8: "B15193",
+    9: None,
+    10: "B15196",
+    20: "B15236",
+    21: None,
+    38: "B15192",
+    111: "B15198",
+    82: None,
+}
+
+STATIONS_URL = "https://docs.google.com/spreadsheets/d/1GlY3Pu9GDpLDk8Spl9yV1wjCRPvOI1m7BFxumfcuGcE/export?format=csv"
+VARIABLES_URL = "https://docs.google.com/spreadsheets/d/13QcqldwA3EQ_4E17Hqggd2ZcMgUA5UwACttAWEkaU28/export?format=csv"
+DATASTORE_URL = "https://dati.arpae.it/api/action/datastore_search?resource_id=a1c46cfe-46e5-44b4-9231-7d9260a38e68"
+
+
+def iter_datastore(low=0, step=100000, high=None):
+    current = low
+    urlparts = list(urlsplit(DATASTORE_URL))
+    querydict = dict(parse_qsl(urlparts[3]))
+    while True:
+        if high is not None and current > high:
+            break
+        else:
+            querydict.update({
+                "limit": step,
+                "offset": current,
+            })
+            urlparts[3] = urlencode(querydict)
+            nexturl = urlunsplit(urlparts)
+            logger.info("Loading data from {}".format(nexturl))
+            resp = json.load(codecs.getreader("utf-8")(urlopen(nexturl)))
+            records = resp["result"]["records"]
+            if len(records) == 0:
+                logging.debug("No records found, stopping")
+                break
+            else:
+                logger.debug("Found {} records".format(len(records)))
+                current += step
+                for r in records:
+                    yield r
+
+
+def load_stations():
+    stations = {}
+    logging.info("Loading stations from {}".format(STATIONS_URL))
+    resp = urlopen(STATIONS_URL)
+    reader = csv.DictReader(codecs.getreader("utf-8")(resp))
+    for row in reader:
+        key = int(row["Cod_staz"])
+        rec = dballe.Record(B01019=row["Stazione"],
+                            B07030=float(row["Altezza"].replace(",", ".")),
+                            lon=float(row["Lon"]), lat=float(row["Lat"]),
+                            rep_memo="arpae-aq")
+        stations[key] = rec
+
+    return stations
+
+
+def load_variables():
+    variables = {}
+    logging.info("Loading variables from {}".format(VARIABLES_URL))
+    resp = urlopen(VARIABLES_URL)
+    reader = csv.DictReader(codecs.getreader("utf-8")(resp))
+    for row in reader:
+        key = int(row["IdParametro"])
+        bcode = VARIABLE_BCODES.get(key)
+        if bcode is None:
+            logger.warning("Var for variable {} not found, skipping".format(key))
+        else:
+            variables[key] = {
+                "var": bcode,
+                "level": (103, 2000),
+                "trange": (0, 0, int(row["Tmed (min)"])*60),
+            }
+
+    return variables
+
+
+def export_data(outfile):
+    db = dballe.DB.connect_from_url("sqlite://:memory:")
+    db.reset()
+    stations = load_stations()
+    variables = load_variables()
+    for rec in stations.values():
+        db.insert_station_data(rec, can_add_stations=True)
+
+    for row in iter_datastore():
+        variable = variables.get(row["variable_id"])
+        station = stations.get(row["station_id"])
+        reftime = datetime.strptime(row["reftime"], "%Y-%m-%dT%H:%M:%S")
+        value = row["value"]
+        if variable is None:
+            logger.warning("Unknown variable {}, skipping".format(row["variable_id"]))
+            continue
+        elif station is None:
+            logger.warning("Unknown station {}, skipping".format(row["station_id"]))
+            continue
+        else:
+            rec = station.copy()
+            rec["date"] = reftime
+            rec[variable["var"]] = value * 10**-9
+            rec["level"] = variable["level"]
+            rec["trange"] = variable["trange"]
+            db.insert_data(rec)
+
+    db.export_to_file(dballe.Record(), filename=outfile,
+                      format="BUFR", generic=True)
+
+
+
+def main():
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--verbose", action="store_true")
+    parser.add_argument("--debug", action="store_true")
+    parser.add_argument("outfile")
+
+    args = parser.parse_args()
+
+    logformat = '%(levelname)s: %(message)s'
+    loglevel = logging.INFO
+
+    if args.verbose:
+        loglevel = logging.INFO
+    elif args.debug:
+        loglevel = logging.DEBUG
+    else:
+        loglevel = logging.WARN
+
+    logging.basicConfig(level=loglevel, format=logformat)
+
+    try:
+        export_data(args.outfile)
+    except Exception as e:
+        logging.exception(e)
+
+
+if __name__ == '__main__':
+    main()
---
+++ arpae_aq_ckan_to_bufr/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name='arpae_aq_ckan_to_bufr',
+    version='0.1.0',
+    description='Python Distribution Utilities',
+    author='Emanuele Di Giacomo',
+    author_email='emanuele@digiacomo.cc',
+    url='https://github.com/r-map/rmap',
+    packages=['arpae_aq_ckan_to_bufr'],
+    license="GNU GPL v2",
+    entry_points={
+        'console_scripts': [
+            'arpae_aq_ckan_to_bufr = arpae_aq_ckan_to_bufr:main'
+        ],
+    },
+)
