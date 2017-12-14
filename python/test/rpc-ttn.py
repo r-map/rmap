0a92adc518f22b83d610038bd397ecae6b2d2cc8
pat1 <p.patruno@iperbole.bologna.it>
2017-12-15 00:34:41 +0100

    test rpc-ttn

---
+++ python/test/rpc-ttn.py
@@ -0,0 +1,14 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+# create JSON-RPC client
+import rmap.jsonrpc as jsonrpc
+server = jsonrpc.ServerProxy(jsonrpc.JsonRpc20(radio=False,notification=False), jsonrpc.TransportSERIAL(port="/dev/ttyUSB0",baudrate=19200, logfunc=jsonrpc.log_file("myrpc.log")))
+
+
+# call a remote-procedure
+print server.set(ack=1)
+print server.set(appeui=(1,2,3,4,5,6,7,8))
+print server.set(deveui=(1,2,3,4,5,6,7,8))
+print server.set(appkey=(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16))
+print server.save(eeprom=True)
