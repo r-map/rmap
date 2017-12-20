f76d1fcf28f2c1170c67593ee7101c704bebb172
dcast78 <dcast78@users.noreply.github.com>
2017-12-20 17:51:44 +0100

    Reorder server.set lines
    Reoder for simply cut and paste data from ttn device setting

--- python/test/rpc-ttn.py
+++ python/test/rpc-ttn.py
@@ -8,8 +8,8 @@ server = jsonrpc.ServerProxy(jsonrpc.JsonRpc20(radio=False,notification=False), 
 
 # call a remote-procedure
 #print server.set(ack=1)
-#print server.set(appeui=(1,2,3,4,5,6,7,8))
 #print server.set(deveui=(1,2,3,4,5,6,7,8))
+#print server.set(appeui=(1,2,3,4,5,6,7,8))
 #print server.set(appkey=(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16))
 #print server.save(eeprom=True)
 
