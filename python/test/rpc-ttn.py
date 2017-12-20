f05692c89e58897da90e97629de0aa9ea2cc955f
dcast78 <dcast78@users.noreply.github.com>
2017-12-20 17:54:45 +0100

    Add note for lsb and msb var format

--- python/test/rpc-ttn.py
+++ python/test/rpc-ttn.py
@@ -8,8 +8,11 @@ server = jsonrpc.ServerProxy(jsonrpc.JsonRpc20(radio=False,notification=False), 
 
 # call a remote-procedure
 #print server.set(ack=1)
+#Note: Use lsb notation for deveui var, use lsb/msb switch button on ttn web page for format adjustement
 #print server.set(deveui=(1,2,3,4,5,6,7,8))
+#Note: Use lsb notation for appeui var, use lsb/msb switch button on ttn web page for format adjustement
 #print server.set(appeui=(1,2,3,4,5,6,7,8))
+#Note: Use msb notation for appkey var, use lsb/msb switch button on ttn web page for format adjustement
 #print server.set(appkey=(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16))
 #print server.save(eeprom=True)
 
