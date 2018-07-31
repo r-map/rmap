c9a008c0c3a0effe03c5f37a36edc891c434c475
Paolo Patruno <ppatruno@arpa.emr.it>
2018-07-31 19:25:02 +0200

    draft for domotic applications

---
+++ arduino/sketchbook/domotica/mycamper/mycamper.ino
@@ -0,0 +1,430 @@
+/*
+ * ESP8266 + BOOTSTRAP + SPIFFS
+ * Copyright (C) 2018 Paolo Paruno <p.patruno@iperbole.bologna.it>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+// increment on change
+#define SOFTWARE_VERSION "2018-07-31T00:00"
+#define FIRMWARE_TYPE "DOMOTICA"
+
+#define SCL D1
+#define SDA D2
+#define RESET_PIN D7    // pin to connect to ground for reset wifi configuration
+#define LED_PIN D4
+
+const char* rmap_server= "rmap.cc";
+#define WIFI_SSED "mycamper"
+#define WIFI_PASSWORD  "ford1234"
+#define OLEDI2CADDRESS 0X3C
+
+// set the frequency
+// 30418,25 Hz  : minimum freq with prescaler set to 1 and CPU clock to 16MHz 
+#define I2C_CLOCK 30418
+
+
+#include <FS.h>                   //this needs to be first, or it all crashes and burns...
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+#include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
+//#include <PubSubClient.h>
+//#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
+#include <SoftwareSerial.h>
+#include <TimeAlarms.h>
+#include <ArduinoLog.h>
+#include <Wire.h>
+#include <U8g2lib.h>
+
+// logging level at compile time
+// Available levels are:
+// LOG_LEVEL_SILENT, LOG_LEVEL_FATAL, LOG_LEVEL_ERROR, LOG_LEVEL_WARNING, LOG_LEVEL_NOTICE, LOG_LEVEL_VERBOSE
+#define LOG_LEVEL   LOG_LEVEL_NOTICE
+
+
+// watchdog is enabled by default on ESP
+// https://techtutorialsx.com/2017/01/21/esp8266-watchdog-functions/
+
+  
+//const char* update_host = "rmap.cc";
+const char* update_url = "/firmware/update/" FIRMWARE_TYPE "/";
+const int update_port = 80;
+
+WiFiClient espClient;
+//PubSubClient mqttclient(espClient);
+
+
+U8G2_SSD1306_64X48_ER_F_HW_I2C u8g2(U8G2_R0);
+bool oledpresent=false;
+unsigned short int displaypos;
+
+const uint8_t GPIOPIN[4] = {D5,D6,D7,D8};  // Led
+
+// Création des objets / create Objects
+ESP8266WebServer server ( 80 );
+DNSServer dnsServer;
+
+void firmware_upgrade() {
+
+  StaticJsonBuffer<200> jsonBuffer; 
+  JsonObject& root = jsonBuffer.createObject();
+  root["ver"] = SOFTWARE_VERSION;
+  char buffer[256];
+  root.printTo(buffer, sizeof(buffer));
+  LOGN(F("url for firmware update: %s" CR),update_url);
+  LOGN(F("version for firmware update: %s" CR),buffer);
+
+  analogWriteFreq(4);
+  analogWrite(LED_PIN,512);  
+
+  //		t_httpUpdate_return ret = ESPhttpUpdate.update(update_host, update_port, update_url, String(SOFTWARE_VERSION) + String(" ") + esp_chipid + String(" ") + SDS_version + String(" ") + String(current_lang) + String(" ") + String(INTL_LANG));
+  t_httpUpdate_return ret = ESPhttpUpdate.update(rmap_server, update_port, update_url, String(buffer));
+  switch(ret)
+    {
+    case HTTP_UPDATE_FAILED:
+      LOGE(F("[update] Update failed." CR));
+      LOGE(F("%s" CR),ESPhttpUpdate.getLastErrorString().c_str());
+      if (oledpresent) {
+	u8g2.setCursor(0, 20); 
+	u8g2.print(F("FW Update"));
+	u8g2.setCursor(0, 30); 
+	u8g2.print(F("Failed"));
+	u8g2.sendBuffer();
+      }
+      digitalWrite(LED_PIN,LOW);      
+      delay(1000);
+      digitalWrite(LED_PIN,HIGH);      
+      delay(1000);
+      digitalWrite(LED_PIN,LOW);      
+      delay(1000);
+    break;
+    case HTTP_UPDATE_NO_UPDATES:
+      LOGN(F("[update] No Update." CR));
+      if (oledpresent) {
+	u8g2.setCursor(0, 20); 
+	u8g2.print(F("NO Firmware"));
+	u8g2.setCursor(0, 30); 
+	u8g2.print(F("Update"));
+	u8g2.sendBuffer();
+      }
+      digitalWrite(LED_PIN,LOW);      
+      delay(1000);
+      break;
+    case HTTP_UPDATE_OK:
+      LOGN(F("[update] Update ok." CR)); // may not called we reboot the ESP
+      /*
+      if (oledpresent) {
+	u8g2.setCursor(0, 20); 
+	u8g2.print(F("FW Updated!"));
+	u8g2.sendBuffer();
+      }
+      digitalWrite(LED_PIN,LOW);      
+      delay(1000);
+      digitalWrite(LED_PIN,HIGH);      
+      delay(1000);
+      digitalWrite(LED_PIN,LOW);      
+      delay(1000);
+      digitalWrite(LED_PIN,HIGH);      
+      delay(1000);
+      digitalWrite(LED_PIN,LOW);      
+      delay(1000);
+      */
+      break;
+    }
+
+  analogWriteFreq(1);
+  digitalWrite(LED_PIN,HIGH);
+
+}
+
+void reboot() {
+  //reset and try again, or maybe put it to deep sleep
+  ESP.restart();
+  delay(5000);
+}
+
+void updateGpio(){
+  String gpio = server.arg("id");
+  String etat = server.arg("etat");
+  String success = "1";
+  int pin = D5;
+  if ( gpio == "D5" ) {
+    pin = D5;
+  } else if ( gpio == "D7" ) {
+    pin = D7;
+  } else if ( gpio == "D8" ) {
+    pin = D8;  
+  } else {   
+      pin = D5;
+  }
+  LOGN(F("pin: %d" CR),pin);
+
+  if ( etat == "1" ) {
+    digitalWrite(pin, HIGH);
+  } else if ( etat == "0" ) {
+    digitalWrite(pin, LOW);
+  } else {
+    success = "1";
+    //LOGE(F("Err pin value: %d" CR),etat);    
+  }
+  
+  String json = "{\"gpio\":\"" + String(gpio) + "\",";
+  json += "\"etat\":\"" + String(etat) + "\",";
+  json += "\"success\":\"" + String(success) + "\"}";
+    
+  server.send(200, "application/json", json);
+  LOGN(F("GPIO updated" CR));
+}
+
+
+void setup() {
+     
+  pinMode(RESET_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+  analogWriteFreq(1);
+  digitalWrite(LED_PIN,HIGH);
+
+  for ( int x = 0 ; x < 5 ; x++ ) {
+    pinMode(GPIOPIN[x], OUTPUT);
+  }
+  
+  Serial.begin ( 115200 );
+  LOGN(F("Started" CR));
+
+  // Pass log level, whether to show log level, and print interface.
+  // Available levels are:
+  // LOG_LEVEL_SILENT, LOG_LEVEL_FATAL, LOG_LEVEL_ERROR, LOG_LEVEL_WARNING, LOG_LEVEL_NOTICE, LOG_LEVEL_VERBOSE
+  // Note: if you want to fully remove all logging code, change #define LOG_LEVEL ....
+  //       this will significantly reduce your project size
+
+  // set runtime log level to the same of compile time
+  Log.begin(LOG_LEVEL, &Serial);
+  LOGN(F("Started" CR));
+  LOGN(F("Version: " SOFTWARE_VERSION CR));
+
+  /*
+  
+#ifdef I2CPULLUP
+  //if you want to set the internal pullup
+  digitalWrite( SDA, HIGH);
+  digitalWrite( SCL, HIGH);
+#else
+  // here we enforce we do not want pullup
+  digitalWrite( SDA, LOW);
+  digitalWrite( SCL, LOW);
+#endif
+
+  Wire.begin(SDA,SCL);
+  Wire.setClock(I2C_CLOCK);
+  */
+
+
+  // check return value of
+  // the Write.endTransmisstion to see if
+  // a device did acknowledge to the address.
+  Wire.beginTransmission(OLEDI2CADDRESS);
+  if (Wire.endTransmission() == 0) {
+    LOGN(F("OLED Found" CR));
+    oledpresent=true;
+    u8g2.setI2CAddress(OLEDI2CADDRESS*2);
+    u8g2.begin();
+    u8g2.setFont(u8g2_font_5x7_tf);
+    u8g2.setFontMode(0); // enable transparent mode, which is faster
+    u8g2.clearBuffer();
+    u8g2.setCursor(0, 10); 
+    u8g2.print(F("Starting up!"));
+    u8g2.setCursor(0, 20); 
+    u8g2.print(F("Version:"));
+    u8g2.setCursor(0, 30); 
+    u8g2.print(F(SOFTWARE_VERSION));
+    u8g2.sendBuffer();
+  }else{
+        LOGN(F("OLED NOT Found" CR));
+  }
+  
+  digitalWrite(LED_PIN,LOW);
+  delay(1000);
+  digitalWrite(LED_PIN,HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN,LOW);
+  delay(1000);
+  digitalWrite(LED_PIN,HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN,LOW);
+  delay(1000);
+  digitalWrite(LED_PIN,HIGH);
+
+
+
+  configTime(0, 0, "0.europe.pool.ntp.org");
+
+  delay(1000);
+  
+  //if (digitalRead(RESET_PIN) == LOW) {
+  if (false) {
+    LOGN(F("Wait for wifi configuration" CR));
+    if (oledpresent) {
+      u8g2.clearBuffer();
+      u8g2.setCursor(0, 10); 
+      u8g2.print(F("Wait conf"));
+      u8g2.sendBuffer();
+      delay(3000);
+    }
+    
+    //WiFiManager
+    //Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wifiManager;
+
+    //wifiManager.resetSettings();
+    
+    //set static ip
+    //wifiManager.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+    
+    //set minimum quality of signal so it ignores AP's under that quality
+    //defaults to 8%
+    //wifiManager.setMinimumSignalQuality();
+    
+    //sets timeout until configuration portal gets turned off
+    //useful to make it all retry or go to sleep
+    //in seconds
+    wifiManager.setTimeout(180);
+
+    
+    if (oledpresent) {
+      u8g2.clearBuffer();
+      u8g2.setCursor(0, 10); 
+      u8g2.print(F("ssed:"));
+      u8g2.setCursor(0, 20); 
+      u8g2.print(F(WIFI_SSED));
+      u8g2.setCursor(0, 35); 
+      u8g2.print(F("password:"));
+      u8g2.setCursor(0, 45); 
+      u8g2.print(F(WIFI_PASSWORD));
+      u8g2.sendBuffer();
+    }
+    
+    analogWrite(LED_PIN,512);
+  
+    //fetches ssid and pass and tries to connect
+    //if it does not connect it starts an access point with the specified name
+    //here  "AutoConnectAP"
+    //and goes into a blocking loop awaiting configuration
+    //wifiManager.setDebugOutput(false);
+				  
+    if (!wifiManager.autoConnect(WIFI_SSED,WIFI_PASSWORD)) {
+      LOGN(F("failed to connect and hit timeout" CR));
+      if (oledpresent) {
+	u8g2.clearBuffer();
+	u8g2.setCursor(0, 10); 
+	u8g2.print(F("WIFI KO"));
+	u8g2.sendBuffer();
+      }
+      delay(3000);
+      reboot();
+    }else{
+  
+      //if you get here you have connected to the WiFi
+      LOGN(F("connected... good!" CR));
+      if (oledpresent) {
+	u8g2.clearBuffer();
+	u8g2.setCursor(0, 10); 
+	u8g2.print(F("WIFI OK"));
+	u8g2.sendBuffer();
+      }
+      digitalWrite(LED_PIN,HIGH);
+      LOGN(F("local ip: %s" CR),WiFi.localIP().toString().c_str());
+    
+    
+      if (oledpresent) {
+	u8g2.setCursor(0, 40); 
+	u8g2.print(F("IP:"));
+	u8g2.setFont(u8g2_font_u8glib_4_tf);
+	u8g2.print(WiFi.localIP().toString().c_str());
+	u8g2.setFont(u8g2_font_5x7_tf);
+	u8g2.sendBuffer();
+      }
+
+      firmware_upgrade();
+    }
+  
+  }
+  
+  //read configuration from FS json
+  LOGN(F("mounting FS..." CR));
+  if (SPIFFS.begin()) {
+  } else {
+    LOGN(F("failed to mount FS" CR));
+    if (oledpresent) {
+      u8g2.clearBuffer();
+      u8g2.setCursor(0, 10); 
+      u8g2.print(F("Failed Mount FS"));
+      u8g2.sendBuffer();
+      delay(3000);
+    }
+    Serial.println();
+  
+    Dir dir = SPIFFS.openDir("/");
+    while (dir.next()) {
+      String fileName = dir.fileName();
+      size_t fileSize = dir.fileSize();
+      Serial.printf("FS File: %s, size: %s\n", fileName.c_str(), String(fileSize).c_str());
+      Serial.println();
+    }
+
+  }
+
+
+  IPAddress apIP(192, 168, 1, 1);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  WiFi.softAP(WIFI_SSED,WIFI_PASSWORD  );
+
+  // modify TTL associated  with the domain name (in seconds)
+  // default is 60 seconds
+  dnsServer.setTTL(300);
+  // set which return code will be used for all other domains (e.g. sending
+  // ServerFailure instead of NonExistentDomain will reduce number of queries
+  // sent by clients)
+  // default is DNSReplyCode::NonExistentDomain
+  dnsServer.setErrorReplyCode(DNSReplyCode::ServerFailure);
+
+
+  // if DNSServer is started with "*" for domain name, it will reply with
+  // provided IP to all DNS request
+  dnsServer.start(53, "*", apIP);
+
+  
+  server.on("/gpio", updateGpio);
+
+  server.serveStatic("/js", SPIFFS, "/js");
+  server.serveStatic("/css", SPIFFS, "/css");
+  server.serveStatic("/img", SPIFFS, "/img");
+  server.serveStatic("/", SPIFFS, "/index.html");
+
+  server.begin();
+  LOGN(F("HTTP server started" CR));
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  dnsServer.processNextRequest();
+  server.handleClient();
+}
+
