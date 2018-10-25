f9ab7e632485c9597d08d1a1c59bdeb07f51a4c5
Paolo Patruno <p.patruno@iperbole.bologna.it>
2018-10-25 11:39:38 +0200

    migrate to django 2

--- python/geoimage/models.py
+++ python/geoimage/models.py
@@ -54,7 +54,7 @@ class GeorefencedImage(models.Model):
     geom = PointField()
     comment = models.TextField()
     #image = DeletingImageField()
-    ident = models.ForeignKey(User)
+    ident = models.ForeignKey(User,on_delete=models.CASCADE)
     date=models.DateTimeField(auto_now=False, auto_now_add=False)
     category = models.CharField(max_length=50, blank=False,choices=CATEGORY_CHOICES)
 
