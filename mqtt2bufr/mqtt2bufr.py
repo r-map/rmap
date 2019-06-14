d10b763fd579aed9a2f89f8f77246983e21600b7
Emanuele Di Giacomo <emanuele@digiacomo.cc>
2019-06-14 10:32:31 +0200

    copyright

--- mqtt2bufr/mqtt2bufr.py
+++ mqtt2bufr/mqtt2bufr.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # mqtt2bufr - Convert MQTT messages to generic BUFR
 #
-# Copyright (C) 2013  ARPA-SIM <urpsim@smr.arpa.emr.it>
+# Copyright (C) 2018  ARPA-SIM <urpsim@smr.arpa.emr.it>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
