27aff16d8fef8dda4a64c36cc973f67a44ee26a4
Paolo Patruno <p.patruno@iperbole.bologna.it>
2019-09-26 23:02:33 +0200

    new release

--- arduino/sketchbook/stima_v2/rmap_esp8266/rmap_esp8266.ino
+++ arduino/sketchbook/stima_v2/rmap_esp8266/rmap_esp8266.ino
@@ -31,7 +31,7 @@ SSL support: Basic SSL"
 
 
 // increment on change
-#define SOFTWARE_VERSION "2019-09-23T12:00"
+#define SOFTWARE_VERSION "2019-09-26T12:00"
 #define FIRMWARE_TYPE ARDUINO_BOARD
 // firmware type for nodemcu is "ESP8266_NODEMCU"
 // firmware type for Wemos D1 mini "ESP8266_WEMOS_D1MINI"
@@ -199,8 +199,12 @@ void handleReport()
 
 
 // web server response function
-void handle_OnConnect() {
-  webserver.send(200, "text/html", SendHTML()); 
+void handle_FullPage() {
+  webserver.send(200, "text/html", FullPage()); 
+}
+
+void handle_Data() {
+  webserver.send(200, "text/html", Data()); 
 }
 
 void handle_NotFound(){
@@ -210,9 +214,67 @@ void handle_NotFound(){
 
 // function to prepare HTML response
 //https://lastminuteengineers.com/esp8266-dht11-dht22-web-server-tutorial/
-String SendHTML(){
+
+String Data(){
+  String str ="<h1>StimaWifi Report</h1>\n";
+  str +="<div class=\"data\">\n";
+  str +="<div class=\"side-by-side temperature-icon\">\n";
+  str +="<svg version=\"1.1\" id=\"Layer_1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\"\n";
+  str +="width=\"9.915px\" height=\"22px\" viewBox=\"0 0 9.915 22\" enable-background=\"new 0 0 9.915 22\" xml:space=\"preserve\">\n";
+  str +="<path fill=\"#FFFFFF\" d=\"M3.498,0.53c0.377-0.331,0.877-0.501,1.374-0.527C5.697-0.04,6.522,0.421,6.924,1.142\n";
+  str +="c0.237,0.399,0.315,0.871,0.311,1.33C7.229,5.856,7.245,9.24,7.227,12.625c1.019,0.539,1.855,1.424,2.301,2.491\n";
+  str +="c0.491,1.163,0.518,2.514,0.062,3.693c-0.414,1.102-1.24,2.038-2.276,2.594c-1.056,0.583-2.331,0.743-3.501,0.463\n";
+  str +="c-1.417-0.323-2.659-1.314-3.3-2.617C0.014,18.26-0.115,17.104,0.1,16.022c0.296-1.443,1.274-2.717,2.58-3.394\n";
+  str +="c0.013-3.44,0-6.881,0.007-10.322C2.674,1.634,2.974,0.955,3.498,0.53z\"></path>\n";
+  str +="</svg>\n";
+  str +="</div>\n";
+  str +="<div class=\"side-by-side temperature-text\">Temperature</div>\n";
+  str +="<div class=\"side-by-side temperature\">";
+  str +=temperature;
+  str +="<span class=\"superscript\">°C</span></div>\n";
+  str +="</div>\n";
+  str +="<div class=\"data\">\n";
+  str +="<div class=\"side-by-side humidity-icon\">\n";
+  str +="<svg version=\"1.1\" id=\"Layer_2\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\"\n";
+  str +="width=\"12px\" height=\"17.955px\" viewBox=\"0 0 13 17.955\" enable-background=\"new 0 0 13 17.955\" xml:space=\"preserve\">\n";
+  str +="<path fill=\"#FFFFFF\" d=\"M1.819,6.217C3.139,4.064,6.5,0,6.5,0s3.363,4.064,4.681,6.217c1.793,2.926,2.133,5.05,1.571,7.057\n";
+  str +="c-0.438,1.574-2.264,4.681-6.252,4.681c-3.988,0-5.813-3.107-6.252-4.681C-0.313,11.267,0.026,9.143,1.819,6.217\"></path>\n";
+  str +="</svg>\n";
+  str +="</div>\n";
+  str +="<div class=\"side-by-side humidity-text\">Humidity</div>\n";
+  str +="<div class=\"side-by-side humidity\">";
+  str +=humidity;
+  str +="<span class=\"superscript\">%</span></div>\n";
+  str +="</div>\n";
+
+  str +="<div class=\"data\">\n";
+  str +="<div class=\"side-by-side temperature-text\">PM2.5</div>\n";
+  str +="<div class=\"side-by-side temperature\">";
+  str +=pm2;
+  str +="<span class=\"superscript\">ug/m3</span></div>\n";
+  str +="</div>\n";
+
+  str +="<div class=\"data\">\n";
+  str +="<div class=\"side-by-side temperature-text\">PM10</div>\n";
+  str +="<div class=\"side-by-side temperature\">";
+  str +=pm10;
+  str +="<span class=\"superscript\">ug/m3</span></div>\n";
+  str +="</div>\n";
+
+  str +="<div class=\"data\">\n";
+  str +="<div class=\"side-by-side temperature-text\">CO2</div>\n";
+  str +="<div class=\"side-by-side temperature\">";
+  str +=co2;
+  str +="<span class=\"superscript\">ppm</span></div>\n";
+  str +="</div>\n";
+
+  return str;
+}
+
+String FullPage(){
   String ptr = "<!DOCTYPE html> <html>\n";
   ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>\n";
   ptr +="<title>StimaWiFI Report</title>\n";
   ptr +="<style>html { display: block; margin: 0px auto; text-align: center;color: #333333;}\n";
   ptr +="body{margin-top: 50px;}\n";
@@ -233,69 +295,20 @@ String SendHTML(){
   ptr +="var xhttp = new XMLHttpRequest();\n";
   ptr +="xhttp.onreadystatechange = function() {\n";
   ptr +="if (this.readyState == 4 && this.status == 200)\n";
-  ptr +="{document.getElementById(\"webpage\").innerHTML =this.responseText}\n";
+  ptr +="{document.getElementById(\"data\").innerHTML =this.responseText}\n";
   ptr +="if (this.readyState == 4 && this.status != 200)\n";
-  ptr +="{document.getElementById(\"webpage\").innerHTML =\"not connected\"}\n";
+  ptr +="{document.getElementById(\"data\").innerHTML =\"not connected\"}\n";
   ptr +="};\n";
-  ptr +="xhttp.open(\"GET\", \"/\", true);\n";
+  ptr +="xhttp.open(\"GET\", \"/data\", true);\n";
   ptr +="xhttp.send();\n";
   ptr +="}\n";
   ptr +="</script>\n";
   ptr +="</head>\n";
   ptr +="<body>\n";
   
-  ptr +="<div id=\"webpage\">\n";
-  
-  ptr +="<h1>StimaWifi Report</h1>\n";
-  ptr +="<div class=\"data\">\n";
-  ptr +="<div class=\"side-by-side temperature-icon\">\n";
-  ptr +="<svg version=\"1.1\" id=\"Layer_1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\"\n";
-  ptr +="width=\"9.915px\" height=\"22px\" viewBox=\"0 0 9.915 22\" enable-background=\"new 0 0 9.915 22\" xml:space=\"preserve\">\n";
-  ptr +="<path fill=\"#FFFFFF\" d=\"M3.498,0.53c0.377-0.331,0.877-0.501,1.374-0.527C5.697-0.04,6.522,0.421,6.924,1.142\n";
-  ptr +="c0.237,0.399,0.315,0.871,0.311,1.33C7.229,5.856,7.245,9.24,7.227,12.625c1.019,0.539,1.855,1.424,2.301,2.491\n";
-  ptr +="c0.491,1.163,0.518,2.514,0.062,3.693c-0.414,1.102-1.24,2.038-2.276,2.594c-1.056,0.583-2.331,0.743-3.501,0.463\n";
-  ptr +="c-1.417-0.323-2.659-1.314-3.3-2.617C0.014,18.26-0.115,17.104,0.1,16.022c0.296-1.443,1.274-2.717,2.58-3.394\n";
-  ptr +="c0.013-3.44,0-6.881,0.007-10.322C2.674,1.634,2.974,0.955,3.498,0.53z\"/>\n";
-  ptr +="</svg>\n";
-  ptr +="</div>\n";
-  ptr +="<div class=\"side-by-side temperature-text\">Temperature</div>\n";
-  ptr +="<div class=\"side-by-side temperature\">";
-  ptr +=temperature;
-  ptr +="<span class=\"superscript\">°C</span></div>\n";
-  ptr +="</div>\n";
-  ptr +="<div class=\"data\">\n";
-  ptr +="<div class=\"side-by-side humidity-icon\">\n";
-  ptr +="<svg version=\"1.1\" id=\"Layer_2\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\"\n\"; width=\"12px\" height=\"17.955px\" viewBox=\"0 0 13 17.955\" enable-background=\"new 0 0 13 17.955\" xml:space=\"preserve\">\n";
-  ptr +="<path fill=\"#FFFFFF\" d=\"M1.819,6.217C3.139,4.064,6.5,0,6.5,0s3.363,4.064,4.681,6.217c1.793,2.926,2.133,5.05,1.571,7.057\n";
-  ptr +="c-0.438,1.574-2.264,4.681-6.252,4.681c-3.988,0-5.813-3.107-6.252-4.681C-0.313,11.267,0.026,9.143,1.819,6.217\"></path>\n";
-  ptr +="</svg>\n";
-  ptr +="</div>\n";
-  ptr +="<div class=\"side-by-side humidity-text\">Humidity</div>\n";
-  ptr +="<div class=\"side-by-side humidity\">";
-  ptr +=humidity;
-  ptr +="<span class=\"superscript\">%</span></div>\n";
-  ptr +="</div>\n";
+  ptr +="<div id=\"data\">\n";
 
-  ptr +="<div class=\"data\">\n";
-  ptr +="<div class=\"side-by-side temperature-text\">PM2.5</div>\n";
-  ptr +="<div class=\"side-by-side temperature\">";
-  ptr +=pm2;
-  ptr +="<span class=\"superscript\">ug/m3</span></div>\n";
-  ptr +="</div>\n";
-
-  ptr +="<div class=\"data\">\n";
-  ptr +="<div class=\"side-by-side temperature-text\">PM10</div>\n";
-  ptr +="<div class=\"side-by-side temperature\">";
-  ptr +=pm10;
-  ptr +="<span class=\"superscript\">ug/m3</span></div>\n";
-  ptr +="</div>\n";
-
-  ptr +="<div class=\"data\">\n";
-  ptr +="<div class=\"side-by-side temperature-text\">CO2</div>\n";
-  ptr +="<div class=\"side-by-side temperature\">";
-  ptr +=co2;
-  ptr +="<span class=\"superscript\">ppm</span></div>\n";
-  ptr +="</div>\n";
+  ptr +=Data();  
 
   ptr +="</div>\n";
   ptr +="</body>\n";
@@ -1274,7 +1287,8 @@ void setup() {
 
 
   // setup web server
-  webserver.on("/", handle_OnConnect);
+  webserver.on("/", handle_FullPage);
+  webserver.on("/data", handle_Data);
   webserver.onNotFound(handle_NotFound);
   
   webserver.begin();
