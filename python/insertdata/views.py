9bad399f02b9a7ce1c7fce4998834d4cd0823e12
Paolo Patruno <p.patruno@iperbole.bologna.it>
2019-10-15 01:45:42 +0200

    nominatim now works

--- python/insertdata/views.py
+++ python/insertdata/views.py
@@ -181,7 +181,7 @@ def insertDataImage(request):
 
             address=nominatimform.cleaned_data['address']
             if address:
-                nom = Nominatim(base_url="http://nominatim.openstreetmap.org",referer= get_current_site(request))
+                nom = Nominatim(base_url="http://nominatim.openstreetmap.org",referer= get_current_site(request).domain)
                 result=nom.query(address,limit=1,countrycodes="IT")
                 if len(result) >= 1:
                     lat= result[0]["lat"]
@@ -409,7 +409,7 @@ def insertDataManualData(request):
 
             address=nominatimform.cleaned_data['address']
             if address:
-                nom = Nominatim(base_url="http://nominatim.openstreetmap.org",referer=get_current_site(request))
+                nom = Nominatim(base_url="http://nominatim.openstreetmap.org",referer=get_current_site(request).domain)
                 result=nom.query(address,limit=1,countrycodes="IT")
                 if result is not None:
                     if len(result) >= 1:
@@ -510,7 +510,7 @@ def insertNewStation(request):
 
             address=nominatimform.cleaned_data['address']
             if address:
-                nom = Nominatim(base_url="http://nominatim.openstreetmap.org",referer=get_current_site(request))
+                nom = Nominatim(base_url="https://nominatim.openstreetmap.org",referer=get_current_site(request).domain)
                 result=nom.query(address,limit=1,countrycodes="IT")
                 if len(result) >= 1:
                     lat= result[0]["lat"]
