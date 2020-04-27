c88bba70e6fb5ae7916dd649111c05874607f8e1
Paolo Patruno <p.patruno@iperbole.bologna.it>
2020-04-27 17:23:45 +0200

    use second i2c bus for STM32

--- platformio/test/oled_ssd1306/src/oled_ssd1306.ino
+++ platformio/test/oled_ssd1306/src/oled_ssd1306.ino
@@ -8,45 +8,69 @@
 
 */
 
+
 // une u8g2 as alternative to more simple u8x8
 #define USE_U8G2
 
+
+#include <Arduino.h>
+#include <Wire.h>
+
+#if defined(ARDUINO_ARCH_STM32)
+#define WIREX Wire1
+TwoWire WIREX(PB4, PA7);
+
+// Define the dimension of the U8*log window
+#define U8LOG_WIDTH 20
+#define U8LOG_HEIGHT 8
+
+#else
+#define WIREX Wire
+
+// Define the dimension of the U8*log window
+#define U8LOG_WIDTH 10
+#define U8LOG_HEIGHT 6
+
+#endif
+
+
 //display definition
 #define OLEDI2CADDRESS 0X3C
+
 // display definitions
 #define fontNameS u8g2_font_tom_thumb_4x6_tf
 #define fontNameB u8g2_font_t0_11_tf
 #define fontName u8x8_font_5x7_f
 
-#include <Arduino.h>
-#include <Wire.h>
-
 #ifdef USE_U8G2
 #include <U8g2lib.h>
 
-U8G2_SSD1306_64X48_ER_F_HW_I2C u8g2(U8G2_R0);
+//U8G2_SSD1306_64X48_ER_F_HW_I2C u8g2(U8G2_R0);
+U8G2_SSD1306_64X48_ER_F_2ND_HW_I2C  u8g2(U8G2_R0);
+
+// Create a U8g2log object
+U8G2LOG u8g2log;
 
 #else
 
 #include <U8x8lib.h>
 
-U8X8_SSD1306_64X48_ER_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
+//U8X8_SSD1306_64X48_ER_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
+U8X8_SSD1306_64X48_ER_2ND_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
 
 // Create a U8x8log object
 U8X8LOG u8x8log;
 
-// Define the dimension of the U8x8log window
-#define U8LOG_WIDTH 10
-#define U8LOG_HEIGHT 6
+#endif
+
 
 // Allocate static memory for the U8x8log window
 uint8_t u8log_buffer[U8LOG_WIDTH*U8LOG_HEIGHT];
 
-#endif
-  
+
 void setup(void)
 {
-  Wire.begin();
+  WIREX.begin();
 
   //The Wire library enables the internal pullup resistors for SDA and SCL.
   //You can turn them off after Wire.begin()
@@ -67,6 +91,11 @@ void setup(void)
   u8g2.print(F("Hello World!"));
   u8g2.sendBuffer();
 
+  // Start U8x8log, connect to U8x8, set the dimension and assign the static memory
+  u8g2log.begin(u8g2, U8LOG_WIDTH, U8LOG_HEIGHT, u8log_buffer);
+  u8g2log.setLineHeightOffset(0);	// set extra space between lines in pixel, this can be negative
+  u8g2log.setRedrawMode(0);		// 0: Update screen with newline, 1: Update screen for every char
+  
 #else  
 
   u8x8.setI2CAddress(OLEDI2CADDRESS*2);
@@ -89,7 +118,11 @@ void setup(void)
 void loop(void)
 {
   delay(1000);
-#ifndef USE_U8G2
+
+  #ifdef USE_U8G2
+  u8g2log.println(millis());
+  #else
   u8x8log.println(millis());
-#endif
+  #endif
+  
 }
