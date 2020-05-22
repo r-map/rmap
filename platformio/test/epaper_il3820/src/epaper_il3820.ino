2bd632a17a37fc3a65b31522225112db5824f7e0
Paolo Patruno <p.patruno@iperbole.bologna.it>
2020-05-22 14:49:16 +0200

    test for epaper 400x300

--- platformio/test/epaper_il3820/src/epaper_il3820.ino
+++ platformio/test/epaper_il3820/src/epaper_il3820.ino
@@ -14,7 +14,7 @@ CLK 	SPI SCK
 CS 	SPI chip select (Low active)
 DC 	Data/Command control pin (High for data, and low for command)
 RST 	External reset pin (Low for reset)
-BUSY 	Busy state output pin (High for busy) 
+BUSY 	Busy state output pin (High for busy)
 
 connect:
 VCC -> 3.3V
@@ -46,22 +46,9 @@ rst -> D10
 #define U8LOG_WIDTH 270
 #define U8LOG_HEIGHT 50
 
-#else
-// Define the dimension of the U8*log window
-#define U8LOG_WIDTH 100
-#define U8LOG_HEIGHT 16
-#endif
-
-
-//display definition
-#define OLEDI2CADDRESS 0X3C
-
-// display definitions
 #define fontNameS u8g2_font_tom_thumb_4x6_tf
 #define fontNameB u8g2_font_t0_11_tf
-#define fontName u8x8_font_5x7_f
 
-#ifdef USE_U8G2
 #include <U8g2lib.h>
 
 //U8G2_IL3820_V2_296X128_F_4W_HW_SPI  u8g2(U8G2_R0,8,9,10);
@@ -70,16 +57,26 @@ U8G2_IL3820_V2_400X300_F_4W_HW_SPI  u8g2(U8G2_R0,8,9,10);
 U8G2LOG u8g2log;
 
 #else
+// Define the dimension of the U8*log window
+//296x128
+//#define U8LOG_WIDTH 100
+//#define U8LOG_HEIGHT 16
+
+//400x300
+#define U8LOG_WIDTH 135
+#define U8LOG_HEIGHT 37
+
+#define fontName u8x8_font_5x7_f
 
 #include <U8x8lib.h>
 
-U8X8_IL3820_V2_296X128_4W_HW_SPI  u8x8(8,9,10);
+//U8X8_IL3820_V2_296X128_4W_HW_SPI  u8x8(8,9,10);
+U8X8_IL3820_V2_400X300_4W_HW_SPI  u8x8(8,9,10);
 
 // Create a U8x8log object
 U8X8LOG u8x8log;
 
 #endif
-
 
 // Allocate static memory for the U8x8log window
 uint8_t u8log_buffer[U8LOG_WIDTH*U8LOG_HEIGHT];
@@ -90,7 +87,7 @@ void setup(void)
 
   // start the SPI library:
   SPI.begin();
-  
+
 #ifdef USE_U8G2
 
   // start up display
@@ -98,31 +95,31 @@ void setup(void)
   u8g2.setFont(fontNameB);
   u8g2.setFontMode(0); // enable transparent mode, which is faster
   u8g2.clearBuffer();
-  u8g2.setCursor(0, 10); 
+  u8g2.setCursor(300, 200);
   u8g2.print(F("Hello World!"));
   u8g2.sendBuffer();
 
-  // Start U8x8log, connect to U8x8, set the dimension and assign the static memory
+  // Start U8g2log, connect to U8x8, set the dimension and assign the static memory
   u8g2log.begin(u8g2, U8LOG_WIDTH, U8LOG_HEIGHT, u8log_buffer);
   u8g2log.setLineHeightOffset(0);	// set extra space between lines in pixel, this can be negative
   u8g2log.setRedrawMode(0);		// 0: Update screen with newline, 1: Update screen for every char
   u8g2.setFont(fontNameS);
-  
-#else  
+
+#else
 
   u8x8.begin();
   u8x8.setPowerSave(0);
 
-  u8x8.setFont(fontNameB);
+  u8x8.setFont(fontName);
   u8x8.drawString(0,0,"Hello");
   u8x8.drawString(0,1,"World!");
   u8x8.refreshDisplay();
-  
+
   // Start U8x8log, connect to U8x8, set the dimension and assign the static memory
   u8x8log.begin(u8x8, U8LOG_WIDTH, U8LOG_HEIGHT, u8log_buffer);
-  
+
   // Set the U8x8log redraw mode
-  u8x8log.setRedrawMode(0);		// 0: Update screen with newline, 1: Update screen for every char  
+  u8x8log.setRedrawMode(0);		// 0: Update screen with newline, 1: Update screen for every char
   u8x8.setFont(fontName);
 
 #endif
@@ -138,5 +135,5 @@ void loop(void)
   u8x8log.println(millis());
   u8x8.refreshDisplay();
   #endif
-  
+
 }
