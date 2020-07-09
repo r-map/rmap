aa692362324d58667a9a64913b6dac42da3733ea
Paolo Patruno <p.patruno@iperbole.bologna.it>
2020-07-09 15:56:41 +0200

    close #312

--- platformio/stima_v3/i2c-rain/src/i2c-rain.ino
+++ platformio/stima_v3/i2c-rain/src/i2c-rain.ino
@@ -128,14 +128,6 @@ void init_power_down(uint32_t *time_ms, uint32_t debouncing_ms) {
 		noInterrupts ();
 		sleep_enable();
 
-
-		// perche non usare void sleep_bod_disable()  ?
-		// http://avr-libc.nongnu.org/user-manual/group__avr__sleep.html#gabf889562cc5ea768ee80cfc8a5bb0312
-		// turn off brown-out
-		#ifdef ARDUINO_AVR_MICRODUINO_CORE_PLUS
-		MCUCR = bit (BODS) | bit (BODSE);
-		MCUCR = bit (BODS);
-		#endif
 		interrupts ();
 
 		sleep_cpu();
