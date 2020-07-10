3022f9d69d860aef3cf00326f315bff94826736b
Marco Baldinetti <m.baldinetti@digiteco.it>
2020-07-10 11:14:57 +0200

    fixes #302 fixes #303

--- pic/digiteco_power/Interrupt.h
+++ pic/digiteco_power/Interrupt.h
@@ -1,3 +1,22 @@
+/*********************************************************************
+Copyright (C) 2017  Marco Baldinetti <m.baldinetti@digiteco.it>
+authors:
+Marco Baldinetti <m.baldinetti@digiteco.it>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of
+the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**********************************************************************/
+
 #ifndef _INTERRUPT_H
 #define _INTERRUPT_H
 #ifdef    __cplusplus
@@ -10,4 +29,3 @@ void RESET_INTERRUPT_FLAG(void);
 }
 #endif
 #endif
-
