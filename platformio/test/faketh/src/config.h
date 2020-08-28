27f30760531bc823d34bd74ee35fcd9a15360d54
Paolo Patruno <p.patruno@iperbole.bologna.it>
2020-08-28 10:46:33 +0200

    added faketh

---
+++ platformio/test/faketh/src/config.h
@@ -0,0 +1,16 @@
+
+#define LEDPIN 13
+#define FORCEDEFAULTPIN 8
+
+// define the version of the configuration saved on eeprom
+// if you chenge this the board start with default configuration at boot
+#define CONFVER "confth00"
+
+// activate debug on serial port
+#define DEBUGONSERIAL
+
+#ifdef DEBUGONSERIAL
+#define IF_SDEBUG(x) ({x;})
+#else
+#define IF_SDEBUG(x)
+#endif
