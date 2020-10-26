93840d4106ac0cb2af22fc8573e1c6b12bf3b612
Paolo Patruno <p.patruno@iperbole.bologna.it>
2020-10-26 13:46:12 +0100

    a working example for uavca v1 with all static ID

--- platformio/test/uavcan-v1/src/example.ino
+++ platformio/test/uavcan-v1/src/example.ino
@@ -17,20 +17,20 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 #include "Arduino.h"
 #include "stm32_def.h"
 #include "canard.h"
+#include "canard_dsdl.h"
 #include "bxcan.h"
 
-static const uint16_t HeartbeatSubjectID      = 7509U;
-static const uint16_t RegisterAccessServiceID = 384;
+static const uint16_t HeartbeatSubjectID         = 7509U;
+static const uint16_t UltrasoundMessageSubjectID = 1610U;
+static const uint16_t RegisterAccessServiceID    = 84U;
+
+CanardInstance canard;
+HardwareSerial Serial2(PA3, PA2);  //uart2
 unsigned long int  next;
-
-/// Should match the wildcard "uavcan.pub.*" for publishers. Likewise, "uavcan.sub.*" for subscribers.
-static const char MeasurementSubjectIDRegisterName[] = "uavcan.pub.measurement";
-
-static uint16_t g_measurement_subject_id = UINT16_MAX;
+unsigned long int  nextrpc;
 
 static void* canardAllocate(CanardInstance* const ins, const size_t amount)
 {
@@ -46,108 +46,115 @@ static void canardFree(CanardInstance* const ins, void* const pointer)
 
 static void publishHeartbeat(CanardInstance* const canard, const uint32_t uptime)
 {
-    static CanardTransferID transfer_id=0;
-    const uint8_t payload[7] =
-      {
-       (uint8_t)(uptime >> 0U),
-       (uint8_t)(uptime >> 8U),
-       (uint8_t)(uptime >> 16U),
-       (uint8_t)(uptime >> 24U),
-       0,   // health 2 bit  // mode   3 bit // vendor_specific_status_code  19 bit
-       0,
-       0,
-      };
-    const CanardTransfer transfer =
-      {
-       .timestamp_usec = micros()+2000000UL,
-       .priority       = CanardPriorityNominal,
-       .transfer_kind  = CanardTransferKindMessage,
-       .port_id        = HeartbeatSubjectID,
-       .remote_node_id = CANARD_NODE_ID_UNSET,
-       .transfer_id    = transfer_id,
-       .payload_size   = sizeof(payload),
-       .payload        = &payload[0],
-      };
-    ++transfer_id;
-    (void) canardTxPush(canard, &transfer);
+  static CanardTransferID transfer_id=0;
+  uint8_t payload[7] ={0};
+  
+  canardDSDLSetUxx(payload,  0,  uptime, 32);
+  canardDSDLSetUxx(payload, 32,  0,       8);
+  canardDSDLSetUxx(payload, 40,  0,       8);
+  canardDSDLSetUxx(payload, 48,  9,       8);
+  
+  const CanardTransfer transfer =
+    {
+     .timestamp_usec = micros()+2000000UL,
+     .priority       = CanardPriorityNominal,
+     .transfer_kind  = CanardTransferKindMessage,
+     .port_id        = HeartbeatSubjectID,
+     .remote_node_id = CANARD_NODE_ID_UNSET,
+     .transfer_id    = transfer_id,
+     .payload_size   = 7, //sizeof(payload),
+     .payload        = &payload[0],
+    };
+  ++transfer_id;
+  if(canardTxPush(canard, &transfer) < 0 )
+    {
+      // An error has occurred: either an argument is invalid or we've ran out of memory.
+      Serial2.println("Error canardTxPush");
+    }
 }
 
 
-static void publishMeasurement(CanardInstance* const canard)
+static void publishUltrasoundMessage(CanardInstance* const canard)
 {
-    // Do not publish messages until the subject-ID is configured.
-    if (g_measurement_subject_id <= CANARD_SUBJECT_ID_MAX)
+  static CanardTransferID transfer_id=0;
+
+  uint8_t payload[4] = {0, 0, 0, 0};
+  //Serilize the distance
+  canardDSDLSetF32(payload, 0, 123.456);
+    
+  const CanardTransfer    transfer =
     {
-        static CanardTransferID transfer_id=0;
-        const CanardTransfer    transfer =
-	  {
-	   .timestamp_usec = micros()+2000000UL,
-	   .priority       = CanardPriorityNominal,
-	   .transfer_kind  = CanardTransferKindMessage,
-	   .port_id        = g_measurement_subject_id,
-	   .remote_node_id = CANARD_NODE_ID_UNSET,
-	   .transfer_id    = transfer_id,
-	   .payload_size   = 13,
-            .payload        = "\x0B\x00"
-	   "So it goes.",
-	  };
-        ++transfer_id;
-        (void) canardTxPush(canard, &transfer);
+     .timestamp_usec = micros()+2000000UL,
+     .priority       = CanardPriorityNominal,
+     .transfer_kind  = CanardTransferKindMessage,
+     .port_id        = UltrasoundMessageSubjectID,
+     .remote_node_id = CANARD_NODE_ID_UNSET,
+     .transfer_id    = transfer_id,
+     .payload_size   = 4, //sizeof(payload),
+     .payload        = &payload[0],
+    };
+  ++transfer_id;
+  if (canardTxPush(canard, &transfer) < 0)
+    {
+      // An error has occurred: either an argument is invalid or we've ran out of memory.
+      Serial2.println("Error canardTxPush");
+    }
+
+}
+
+
+static void requestRpc(CanardInstance* const canard)
+{
+  static CanardTransferID transfer_id=0;
+
+  #ifdef BOARD1
+  CanardNodeID remote_node_id        = (CanardNodeID) 11;
+  #endif
+
+  #ifdef BOARD2
+  CanardNodeID remote_node_id        = (CanardNodeID) 10;
+  #endif
+
+  const CanardTransfer    transfer =
+    {
+     .timestamp_usec = micros()+2000000UL,
+     .priority       = CanardPriorityNominal,
+     .transfer_kind  = CanardTransferKindRequest,
+     .port_id        = RegisterAccessServiceID,
+     .remote_node_id = remote_node_id,
+     .transfer_id    = transfer_id,
+     .payload_size   = 0,
+     .payload        = NULL,
+    };
+  ++transfer_id;
+  if (canardTxPush(canard, &transfer) < 0)
+    {
+      // An error has occurred: either an argument is invalid or we've ran out of memory.
+      Serial2.println("Error canardTxPush");
     }
 }
 
-static void handleRegisterAccess(CanardInstance* const canard, const CanardTransfer* const request_transfer)
+
+
+static void handleRequestTransfer(CanardInstance* const canard, const CanardTransfer* const request_transfer)
 {
-  // The following few lines is a basic hand-crafted special-purpose deserializer.
-  const uint8_t* pld      = (uint8_t*)request_transfer->payload;
-  size_t         pld_size = request_transfer->payload_size;
-  const uint8_t  name_len = *pld++;
-  pld_size--;
-  const uint8_t* const name = pld;
-  if (name_len >= pld_size)
-    {
-      return;  // Bad serialization
-    }
-  pld += name_len;
-  pld_size -= name_len;
-  const uint8_t value_tag = *pld++;
-  pld_size--;
-  uint8_t response[313] = {0};  // All-zero response encodes an empty Value, which represents an invalid register.
-  size_t  response_size = 9;    // timestamp [7] + flags [1] + value tag [1] + empty value [0]
-  
-  // Match the name against existing registers.
-  // If none match, respond with an empty value, thereby indicating the lack of such register.
-  if ((name_len == sizeof(MeasurementSubjectIDRegisterName) - 1) &&
-      (0 == memcmp(MeasurementSubjectIDRegisterName, name, name_len)))
-    {
-      // Write the register (if the type matches).
-      if ((value_tag == 10) && (pld_size >= 3) && (*pld == 1))  // Natural16[1]
-        {
-	  ++pld;
-	  uint16_t value = *pld++;
-	  value |= *pld++ << 8;
-	  pld_size -= 3;
-	  g_measurement_subject_id = value;
-        }
-        // Then read it and return the result.
-      response[7]   = 0b00000001;  // Flags: mutable, not persistent (although normally it should be persistent)
-      response[8]   = 10;          // Type: Natural16
-      response[9]   = 1;           // One element
-      response[10]  = (uint8_t)(g_measurement_subject_id);
-      response[11]  = (uint8_t)(g_measurement_subject_id >> 8);
-      response_size = 12;
-    }
-  
   // Send the response back. Make sure to re-use the same priority and transfer-ID.
+  uint8_t payload[4] = {0, 0, 0, 0};
+  //Serialize the temperature
+  canardDSDLSetF32(payload, 0, 273.15);
+
   CanardTransfer response_transfer = *request_transfer;
+  response_transfer.timestamp_usec = micros()+2000000UL,
   response_transfer.transfer_kind  = CanardTransferKindResponse;
-  response_transfer.payload_size   = response_size;
-  response_transfer.payload        = response;
-  
-  (void) canardTxPush(canard, &response_transfer);
+  response_transfer.payload_size = 4; // sizeof(payload)
+  response_transfer.payload = &payload[0];   
+
+  if (canardTxPush(canard, &response_transfer) < 0)
+    {
+      // An error has occurred: either an argument is invalid or we've ran out of memory.
+      Serial2.println("Error canardTxPush");
+    }
 }
-
-
 
 void CAN_HW_Init(void) {
 
@@ -198,47 +205,7 @@ void CAN_HW_Init(void) {
 }
 
 
-CanardInstance canard;
-
-void setup(void) {
-
-  Serial.begin(115200);
-  Serial.print("Initializing...");
-  Serial.println(HAL_RCC_GetHCLKFreq());
-
-  // initialize digital pins
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  CAN_HW_Init();
-  
-  // Initialize the node with a static node-ID.
-  canard = canardInit(&canardAllocate, &canardFree);
-  canard.mtu_bytes      = CANARD_MTU_CAN_CLASSIC;  // Do not use CAN FD
-  canard.node_id        = (CanardNodeID) 10;
-  
-  // Configure the library to listen for register access service requests.
-  CanardRxSubscription srv_register_access;
-  (void) canardRxSubscribe(&canard,
-			   CanardTransferKindRequest,
-			   RegisterAccessServiceID,
-			   1024U,  // Larger buffers are OK.
-			   CANARD_DEFAULT_TRANSFER_ID_TIMEOUT_USEC,
-			   &srv_register_access);
-  
-  next = millis() + 1000;
-}
-
-
-void loop(void) {  
-  // The main loop: publish messages and process service requests.
-  if (next < millis()) {
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    publishMeasurement(&canard);
-    publishHeartbeat(&canard, next);
-    next = next + 1000;
-  }
-  
+void sendQueued(void){
   // Transmit pending frames.
   const CanardFrame* txf = canardTxPeek(&canard);
   while (txf != NULL) {
@@ -250,11 +217,14 @@ void loop(void) {
 		  txf->payload)){
       
       canardTxPop(&canard);
+      //canard.memory_free(&canard,(void*) txf);
       free((void*) txf);
       txf = canardTxPeek(&canard);
     }
   }
-  
+}
+
+void processReceived(){
   // Process received frames, if any.
   uint32_t  out_extended_can_id;
   size_t    out_payload_size;
@@ -264,6 +234,12 @@ void loop(void) {
 		  &out_extended_can_id,
 		  &out_payload_size,
 		  out_payload)) {
+    /*
+    Serial2.print("packet received: ");
+    Serial2.print(out_extended_can_id);
+    Serial2.print(" : ");
+    Serial2.println(out_payload_size);
+    */
     CanardFrame rxf=
       {
        .extended_can_id=out_extended_can_id,
@@ -272,12 +248,157 @@ void loop(void) {
       };
 
     CanardTransfer transfer;
-    if (canardRxAccept(&canard, &rxf, 0, &transfer)) {
-      if ((transfer.transfer_kind == CanardTransferKindRequest) &&
-	  (transfer.port_id == RegisterAccessServiceID)) {
-	handleRegisterAccess(&canard, &transfer);
+    const int8_t result = canardRxAccept(&canard, &rxf, 0, &transfer);
+    if ( result == 1 ){
+      /*
+      Serial2.print("transfer kind and port_id: ");
+      Serial2.print(transfer.transfer_kind);
+      Serial2.print(" : ");
+      Serial2.println(transfer.port_id);
+      */
+      if ((transfer.transfer_kind == CanardTransferKindMessage) &&
+	  (transfer.port_id == HeartbeatSubjectID)) {
+	uint8_t  mode   = canardDSDLGetU8((const uint8_t*)transfer.payload,  transfer.payload_size, 40,  8);
+	uint32_t uptime = canardDSDLGetU32((const uint8_t*)transfer.payload, transfer.payload_size,  0, 32);
+	uint8_t  vssc   = canardDSDLGetU32((const uint8_t*)transfer.payload, transfer.payload_size, 48,  8);
+	uint8_t  health = canardDSDLGetU8((const uint8_t*)transfer.payload,  transfer.payload_size, 32,  8);
+
+	Serial2.print("Heartbeat -> ");
+	Serial2.print("uptime: ");
+	Serial2.print(uptime);
+	Serial2.print(" mode: ");
+	Serial2.print(mode);
+	Serial2.print(" health: ");
+	Serial2.print(health);
+	Serial2.print(" vssc: ");
+	Serial2.println(vssc);
+
+      } else if ((transfer.transfer_kind == CanardTransferKindMessage) &&
+		 (transfer.port_id == UltrasoundMessageSubjectID)) {
+	// Deserialize distance from ultrasound message
+	CanardDSDLFloat32 distance = canardDSDLGetF32((const uint8_t*)transfer.payload, transfer.payload_size, 0);          
+	
+	Serial2.print("UltrasoundMessage -> ");
+	Serial2.print("distance: ");
+	Serial2.println(distance);
+
+      } else if ((transfer.transfer_kind == CanardTransferKindRequest) &&
+		 (transfer.port_id == RegisterAccessServiceID)) {
+	handleRequestTransfer(&canard, &transfer);
+      } else if ((transfer.transfer_kind == CanardTransferKindResponse) &&
+		 (transfer.port_id == RegisterAccessServiceID)) {
+	// Deserialize distance from rpc response
+	CanardDSDLFloat32 temperature = canardDSDLGetF32((const uint8_t*)transfer.payload, transfer.payload_size, 0);          
+	Serial2.print("RPC Message -> ");
+	Serial2.print("temperature: ");
+	Serial2.println(temperature);
+      } else {
+	Serial2.println("transfer kind and port_id not managed");
       }
+      //canard.memory_free(&canard,(void*) transfer.payload);
       free((void*) transfer.payload);
+    }else if ( result < 0 ) {
+      Serial2.println("An error has occurred: either an argument is invalid or we've ran out of memory");
+    } else {
+      Serial2.println("The received frame is either invalid or it's a non-last frame of a multi-frame transfer.");
     }
   }
+}
+
+
+void setup(void) {
+
+  Serial2.begin(115200);
+  Serial2.print("Initializing...");
+  Serial2.println(HAL_RCC_GetHCLKFreq());
+
+  // initialize digital pins
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  CAN_HW_Init();
+ 
+  // Initialize the node with a static node-ID.
+  canard = canardInit(&canardAllocate, &canardFree);
+  canard.mtu_bytes      = CANARD_MTU_CAN_CLASSIC;  // Do not use CAN FD
+
+  #ifdef BOARD1
+  canard.node_id        = (CanardNodeID) 10;
+  #endif
+
+  #ifdef BOARD2
+  canard.node_id        = (CanardNodeID) 11;
+  #endif
+ 
+  // Configure the library to listen for register access service requests.
+  static CanardRxSubscription heartbeat_subscription;
+  (void) canardRxSubscribe(&canard, // Subscribe to messages uavcan.node.Heartbeat.
+			   CanardTransferKindMessage,
+			   HeartbeatSubjectID, // The fixed Subject-ID of the Heartbeat message type (see DSDL definition).
+			   12,                  // The maximum payload size (max DSDL object size) from the DSDL definition.
+			   CANARD_DEFAULT_TRANSFER_ID_TIMEOUT_USEC,
+			   &heartbeat_subscription);
+
+  // Ultrasound subscription
+  static CanardRxSubscription ultrasound_subscription;
+  (void)canardRxSubscribe(&canard,
+			  CanardTransferKindMessage,
+			  UltrasoundMessageSubjectID,
+			  10,                  
+			  CANARD_DEFAULT_TRANSFER_ID_TIMEOUT_USEC,
+			  &ultrasound_subscription);
+
+  // rpc subscriptions
+  static CanardRxSubscription rpc_request_subscription;
+  (void)canardRxSubscribe(&canard,
+			  CanardTransferKindRequest,
+			  RegisterAccessServiceID,  
+			  10,                  
+			  CANARD_DEFAULT_TRANSFER_ID_TIMEOUT_USEC,
+			  &rpc_request_subscription);
+
+  static CanardRxSubscription rpc_response_subscription;
+  (void)canardRxSubscribe(&canard,
+			  CanardTransferKindResponse,
+			  RegisterAccessServiceID,  
+			  10,                  
+			  CANARD_DEFAULT_TRANSFER_ID_TIMEOUT_USEC,
+			  &rpc_response_subscription);
+
+  next = millis() + 1000;
+  nextrpc = millis() + 2500;
+}
+
+// The main loop: publish messages and process service requests.
+void loop(void) {  
+  if (next <= millis()) {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    /*
+    Serial2.print("It's time to send ");
+    Serial2.print(millis());
+    Serial2.print(":");
+    Serial2.println(next);
+    */
+    publishUltrasoundMessage(&canard);
+    publishHeartbeat(&canard, millis());
+    next = next + 1000;
+  }
+
+  sendQueued();
+  processReceived();
+  
+  if (nextrpc <= millis()) {
+    /*    
+    Serial2.print("It's time for rpc ");
+    Serial2.print(millis());
+    Serial2.print(":");
+    Serial2.println(nextrpc);
+    */
+    requestRpc(&canard);
+    nextrpc = nextrpc + 3000;
+  }
+  
+  sendQueued();
+  processReceived();
+  
 }
