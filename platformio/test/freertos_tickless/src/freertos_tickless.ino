72a42d3b75d638c1d20ccad7879726e6a97887b4
Paolo Patruno <p.patruno@iperbole.bologna.it>
2021-01-14 12:46:41 +0100

    added LPTIM1 management; do not work

--- platformio/test/freertos_tickless/src/freertos_tickless.ino
+++ platformio/test/freertos_tickless/src/freertos_tickless.ino
@@ -17,27 +17,64 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ***************************************************************************/
 
 #include "STM32FreeRTOS.h"
+#include "stm32l4xx_hal_lptim.h"
+
+
+LPTIM_HandleTypeDef hlptim1;
+//RTC_HandleTypeDef hrtc;
+
+static void MX_LPTIM1_Init(void)
+{
+  hlptim1.Instance = LPTIM1;
+  hlptim1.Init.Clock.Source = LPTIM_CLOCKSOURCE_APBCLOCK_LPOSC;
+  hlptim1.Init.Clock.Prescaler = LPTIM_PRESCALER_DIV1;
+  hlptim1.Init.Trigger.Source = LPTIM_TRIGSOURCE_SOFTWARE;
+  hlptim1.Init.OutputPolarity = LPTIM_OUTPUTPOLARITY_HIGH;
+  hlptim1.Init.UpdateMode = LPTIM_UPDATE_IMMEDIATE;
+  hlptim1.Init.CounterSource = LPTIM_COUNTERSOURCE_INTERNAL;
+  hlptim1.Init.Input1Source = LPTIM_INPUT1SOURCE_GPIO;
+  hlptim1.Init.Input2Source = LPTIM_INPUT2SOURCE_GPIO;
+  if (HAL_LPTIM_Init(&hlptim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/*
+static void MX_RTC_Init(void)
+{
+  hrtc.Instance = RTC;
+  hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
+  hrtc.Init.AsynchPrediv = 127;
+  hrtc.Init.SynchPrediv = 255;
+  hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
+  hrtc.Init.OutPutRemap = RTC_OUTPUT_REMAP_NONE;
+  hrtc.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
+  hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
+  if (HAL_RTC_Init(&hrtc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+*/
 
 void taskOne( void * parameter )
 {
   while( true ){ 
-    digitalToggle(LED_BUILTIN);    
+    digitalToggle(LED_BUILTIN);
+    //delay(1000);
     vTaskDelay(1000/portTICK_PERIOD_MS);
   }
 }
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);   
- 
-  xTaskCreate(
-	      taskOne,          /* Task function. */
-	      "TaskOne",        /* String with name of task. */
-	      200,              /* Stack size in bytes. */
-	      NULL,             /* Parameter passed as input of the task */
-	      1,                /* Priority of the task. */
-	      NULL);            /* Task handle. */
 
-  vTaskStartScheduler(); // initialise and run the freeRTOS scheduler. Execution should never return here.
+  MX_LPTIM1_Init();
+  //MX_RTC_Init();
   
+  pinMode(LED_BUILTIN, OUTPUT);   
+  xTaskCreate(taskOne,"TaskOne",200,NULL,1,NULL);
+  vTaskStartScheduler(); // initialise and run the freeRTOS scheduler.
+  //Execution should never return here.
 }
 
 void loop()
