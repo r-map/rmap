aec808a5c95f3b54d7cdbde09db698d2dce01d62
Paolo Patruno <p.patruno@iperbole.bologna.it>
2022-09-13 21:17:11 +0200

    compile pipe2dba

--- pipe2dba/compila.sh
+++ pipe2dba/compila.sh
@@ -1,2 +1,3 @@
-gfortran pipe2dba.F03 -I /usr/lib/gfortran/modules/ -lsim_vol7d -lsim_base -llog4fortran -o pipe2dba
+cp pipe2dba.F03.passa_da_metaanddatav pipe2dba.F03
+gfortran pipe2dba.F03 -I /usr/lib64/gfortran/modules/ -lsim_vol7d -lsim_base -llog4fortran -llog4c -o pipe2dba
 
