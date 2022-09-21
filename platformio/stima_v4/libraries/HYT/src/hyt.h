83fddcf6f6aadc36e3b414d58b69accdf14fb2b6
Marco Baldinetti <marcobaldinetti@gmail.com>
2022-09-21 18:49:32 +0200

    corrette informazioni su licenza

--- platformio/stima_v4/libraries/HYT/src/hyt.h
+++ platformio/stima_v4/libraries/HYT/src/hyt.h
@@ -1,23 +1,32 @@
-/**@file hyt2x1.h */
-
-/*********************************************************************
-Copyright (C) 2017  Marco Baldinetti <m.baldinetti@digiteco.it>
-authors:
-Marco Baldinetti <m.baldinetti@digiteco.it>
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation; either version 2 of
-the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-**********************************************************************/
+/**
+* @file hyt.h
+* @brief Main
+*
+* @section License
+*
+* SPDX-License-Identifier: GPL-2.0-or-later
+*
+* Copyright (C) 2022 Marco Baldinetti. All rights reserved.
+*
+* This file is part of CycloneTCP Open.
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software Foundation,
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*
+* @author Marco Baldinetti <marco.baldinetti@alling.it>
+* @version 1.0.0
+**/
 
 #ifndef _HYT_H
 #define _HYT_H
