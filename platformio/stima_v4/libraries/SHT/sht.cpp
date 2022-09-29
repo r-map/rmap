7727170c20961bc2cd98e23e547cb0930bb700cb
Marco Baldinetti <marcobaldinetti@gmail.com>
2022-09-29 16:37:15 +0200

    libreria SHT riscritta ad oggetti su base della arduino-sht

--- platformio/stima_v4/libraries/SHT/sht.cpp
+++ platformio/stima_v4/libraries/SHT/sht.cpp
@@ -1,32 +1,28 @@
-/**
-* @file sht.cpp
-* @brief Main
-*
-* @section License
-*
-* SPDX-License-Identifier: GPL-2.0-or-later
-*
-* Copyright (C) 2022 Marco Baldinetti. All rights reserved.
-*
-* This file is part of CycloneTCP Open.
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
-* of the License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software Foundation,
-* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*
-* @author Marco Baldinetti <marco.baldinetti@alling.it>
-* @version 1.0.0
-**/
+/**@file sht.cpp */
+
+/*********************************************************************
+Copyright (C) 2022  Marco Baldinetti <marco.baldinetti@alling.it>
+authors:
+Marco Baldinetti <marco.baldinetti@alling.it>
+
+This is based on popular lib "arduino-sht"
+<https://github.com/Sensirion/arduino-sht>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+<http://www.gnu.org/licenses/>.
+**********************************************************************/
 
 #include "sht.h"
 
@@ -48,6 +44,7 @@ uint8_t Sht::getAcquisitionDelayMs() {
     case SHT_HIGH_ACCURACY_MODE: acq_duration_ms = SHT_HIGH_ACCURACY_CONVERSION_TIME_MS; break;
     case SHT_MEDIUM_ACCURACY_MODE: acq_duration_ms = SHT_MEDIUM_ACCURACY_CONVERSION_TIME_MS; break;
     case SHT_LOW_ACCURACY_MODE: acq_duration_ms = SHT_LOW_ACCURACY_CONVERSION_TIME_MS; break;
+    default: acq_duration_ms = SHT_HIGH_ACCURACY_CONVERSION_TIME_MS; break;
   }
 
   return acq_duration_ms;
--- platformio/stima_v4/libraries/SHT/sht.h
+++ platformio/stima_v4/libraries/SHT/sht.h
@@ -1,32 +1,28 @@
-/**
-* @file sht.h
-* @brief Main
-*
-* @section License
-*
-* SPDX-License-Identifier: GPL-2.0-or-later
-*
-* Copyright (C) 2022 Marco Baldinetti. All rights reserved.
-*
-* This file is part of CycloneTCP Open.
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
-* of the License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software Foundation,
-* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*
-* @author Marco Baldinetti <marco.baldinetti@alling.it>
-* @version 1.0.0
-**/
+/**@file sht.h */
+
+/*********************************************************************
+Copyright (C) 2022  Marco Baldinetti <marco.baldinetti@alling.it>
+authors:
+Marco Baldinetti <marco.baldinetti@alling.it>
+
+This is based on popular lib "arduino-sht"
+<https://github.com/Sensirion/arduino-sht>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+<http://www.gnu.org/licenses/>.
+**********************************************************************/
 
 #ifndef _SHT_H
 #define _SHT_H
