4432b06f57e32019728de65af48035d5f6fb8d09
Marco Baldinetti <marcobaldinetti@gmail.com>
2022-11-02 18:14:23 +0100

    working freertos

---
+++ platformio/stima_v4/hardware master-stimav4/include/task_config.h
@@ -0,0 +1,50 @@
+/**@file task_config.h */
+
+/*********************************************************************
+Copyright (C) 2022  Marco Baldinetti <marco.baldinetti@alling.it>
+authors:
+Marco Baldinetti <marco.baldinetti@alling.it>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+<http://www.gnu.org/licenses/>.
+**********************************************************************/
+
+#ifndef _TASK_CONFIG_H
+#define _TASK_CONFIG_H
+
+#undef OS_TASK_PRIORITY_IDLE
+#undef OS_TASK_PRIORITY_NORMAL
+#undef OS_TASK_PRIORITY_HIGH
+
+#define OS_TASK_PRIORITY_00     (tskIDLE_PRIORITY)
+#define OS_TASK_PRIORITY_01     (tskIDLE_PRIORITY + 1)
+#define OS_TASK_PRIORITY_02     (tskIDLE_PRIORITY + 2)
+#define OS_TASK_PRIORITY_03     (tskIDLE_PRIORITY + 3)
+#define OS_TASK_PRIORITY_04     (tskIDLE_PRIORITY + 4)
+#define OS_TASK_PRIORITY_05     (tskIDLE_PRIORITY + 5)
+#define OS_TASK_PRIORITY_06     (tskIDLE_PRIORITY + 6)
+#define OS_TASK_PRIORITY_07     (tskIDLE_PRIORITY + 7)
+#define OS_TASK_PRIORITY_08     (tskIDLE_PRIORITY + 8)
+#define OS_TASK_PRIORITY_09     (tskIDLE_PRIORITY + 9)
+#define OS_TASK_PRIORITY_10     (tskIDLE_PRIORITY + 10)
+#define OS_TASK_PRIORITY_11     (tskIDLE_PRIORITY + 11)
+#define OS_TASK_PRIORITY_12     (tskIDLE_PRIORITY + 12)
+#define OS_TASK_PRIORITY_13     (tskIDLE_PRIORITY + 13)
+
+#define OS_TASK_PRIORITY_IDLE   (OS_TASK_PRIORITY_00)
+#define OS_TASK_PRIORITY_NORMAL (OS_TASK_PRIORITY_02)
+#define OS_TASK_PRIORITY_HIGH   (OS_TASK_PRIORITY_10)
+
+#endif
--- platformio/stima_v4/hardware master-stimav4/src/module_master.ino
+++ platformio/stima_v4/hardware master-stimav4/src/module_master.ino
@@ -4,49 +4,63 @@
 // Copyright (C) 2022 Digiteco s.r.l.
 // Author: Gasperini Moreno <m.gasperini@digiteco.it>
 
+#define TRACE_LEVEL STIMA_TRACE_LEVEL
+
 // Arduino
-#include <Arduino.h>
-// Private HW Configuration
 #include "module_master_hal.hpp"
+#include "task_config.h"
+#include <STM32FreeRTOS.h>
+#include "thread.hpp"
+// #include "semaphore.hpp"
+// #include "queue.hpp"
+// #include "stm32l4xx_hal.h"
+// Private HW Configuration
+// #include <Arduino.h>
+// #include "eeprom.h
+#include "tasks/prova_task.h"
+
 
 // *********************************************************************************************
 //                                       SETUP AMBIENTE
 // *********************************************************************************************
 void setup(void) {
 
-    // *****************************************************
-    //   STARTUP PRIVATE BASIC HARDWARE CONFIG AND ISTANCE
-    // *****************************************************
-    MX_GPIO_Init();
-    MX_CAN1_Init();
-    MX_CRC_Init();
-    MX_I2C2_Init();
-    MX_QUADSPI_Init();
-    MX_RTC_Init();
-    MX_UART4_Init();
-    MX_USART1_UART_Init();
-    MX_LPTIM1_Init();
-    MX_LPTIM2_Init();
-    MX_RNG_Init();
-    MX_TIM3_Init();
-    MX_USART2_UART_Init();
-    MX_I2C1_Init();
-    MX_SPI1_Init();
+  // *****************************************************
+  //   STARTUP PRIVATE BASIC HARDWARE CONFIG AND ISTANCE
+  // *****************************************************
+  MX_GPIO_Init();
+  MX_CAN1_Init();
+  MX_CRC_Init();
+  MX_I2C2_Init();
+  MX_QUADSPI_Init();
+  MX_RTC_Init();
+  MX_UART4_Init();
+  MX_USART1_UART_Init();
+  MX_LPTIM1_Init();
+  MX_LPTIM2_Init();
+  MX_RNG_Init();
+  MX_TIM3_Init();
+  MX_USART2_UART_Init();
+  MX_I2C1_Init();
+  MX_SPI1_Init();
 
-    Serial.begin(115200);
-    // Wait for serial port to connect
-    while (!Serial) {
-    }
-    Serial.println(F("Start RS232 Monitor"));
+  Serial.begin(115200);
+  // Wait for serial port to connect
+  while (!Serial) {
+  }
+  Serial.println(F("Start RS232 Monitor"));
 
-    // *****************************************************
-    //            STARTUP LED E PIN DIAGNOSTICI
-    // *****************************************************
-    // Output mode for LED BLINK SW LOOP (High per Setup)
-    // Input mode for test button
-    
-    Serial.println(F("Initialization HW Base done"));
+  // *****************************************************
+  //            STARTUP LED E PIN DIAGNOSTICI
+  // *****************************************************
+  // Output mode for LED BLINK SW LOOP (High per Setup)
+  // Input mode for test button
 
+  Serial.println(F("Initialization HW Base done"));
+
+  ProvaParam_t provaParam = {};
+  static ProvaTask prova_task("PROVA TASK", 100, OS_TASK_PRIORITY_01, provaParam);
+  cpp_freertos::Thread::StartScheduler();
 }
 
 
@@ -57,38 +71,53 @@ void setup(void) {
 /* Get the rtc object */
 //STM32RTC& rtc = STM32RTC::getInstance();
 
-void loop(void)
-{
-  RTC_TimeTypeDef sTime = {0};
-  RTC_DateTypeDef sDate = {0};
-  byte last_ss=100;
+void loop(void) {
+  // BinarySemaphore *wireLock;
+  // wireLock = new BinarySemaphore(true);
 
-//    LowPower.enableWakeupFrom(&rtc, cbf, &atime);
+  // uint8_t write_buffer[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  // uint8_t read_buffer[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-    // rtc.setClockSource(STM32RTC::LSE_CLOCK);
-    // rtc.begin(); // initialize RTC 24H format
-    // rtc.setTime(10, 0, 0);
-    // rtc.setDate(1, 17, 10, 22);
+  // EEprom eeprom = EEprom();
 
-// Configure low power
-//   LowPower.begin();
-//   LowPower.enableWakeupFrom(&rtc, alarmMatch, &atime);
+  // // eeprom.Write(0, write_buffer, 10);
+  // delay(100);
+  // eeprom.Read(0, read_buffer, 10);
 
-//   // Configure first alarm in 2 second then it will be done in the rtc callback
-//   rtc.setAlarmEpoch( rtc.getEpoch() + 2);
+  // Serial.println("Read");
+  // for (uint8_t i=0; i<10; i++) {
+  //   Serial.println(read_buffer[i]);
+  // }
+  // delay(5000);
 
-    do {
-        if(HAL_RTC_GetTime(&hrtc, &sTime, RTC_FORMAT_BCD)!= HAL_OK) {
-            delay(300);
-            Serial.println(F("Error!!!,"));
-        }
-        if(sTime.Seconds!=last_ss) {
-            last_ss=sTime.Seconds;
-            Serial.print(F("Event second... BCD -> "));
-            Serial.println(sTime.Seconds);                  
-            //LowPower.deepSleep();
-        }
-
-    } while (1);
-
+  // RTC_TimeTypeDef sTime = {0};
+  // RTC_DateTypeDef sDate = {0};
+  // byte last_ss=100;
+  //
+  // LowPower.enableWakeupFrom(&rtc, cbf, &atime);
+  //
+  // rtc.setClockSource(STM32RTC::LSE_CLOCK);
+  // rtc.begin(); // initialize RTC 24H format
+  // rtc.setTime(10, 0, 0);
+  // rtc.setDate(1, 17, 10, 22);
+  //
+  // Configure low power
+  // LowPower.begin();
+  // LowPower.enableWakeupFrom(&rtc, alarmMatch, &atime);
+  //
+  // // Configure first alarm in 2 second then it will be done in the rtc callback
+  // rtc.setAlarmEpoch( rtc.getEpoch() + 2);
+  //
+  // do {
+  //   if(HAL_RTC_GetTime(&hrtc, &sTime, RTC_FORMAT_BCD)!= HAL_OK) {
+  //     delay(300);
+  //     Serial.println(F("Error!!!,"));
+  //   }
+  //   if(sTime.Seconds!=last_ss) {
+  //     last_ss=sTime.Seconds;
+  //     Serial.print(F("Event second... BCD -> "));
+  //     Serial.println(sTime.Seconds);
+  //     //LowPower.deepSleep();
+  //   }
+  // } while (1);
 }
