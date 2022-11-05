e55353c9dd0914c87e2102865f752946bd518d11
Moreno Gasperini <m.gasperini@digiteco.it>
2022-11-05 19:53:43 +0100

    Master Freertos CallBack
    Aggiunta CallBack di Freertos Handler e Power

--- platformio/stima_v4/master/include/STM32FreeRTOSConfig_extra.h
+++ platformio/stima_v4/master/include/STM32FreeRTOSConfig_extra.h
@@ -1,2 +1,11 @@
 #define configUSE_CMSIS_RTOS_V2        1
 #define configCHECK_FOR_STACK_OVERFLOW 1
+
+#define _USE_FREERTOS_LOW_POWER
+#ifdef _USE_FREERTOS_LOW_POWER
+    #define _EXIT_SLEEP_FOR_DEBUGGING
+    #define configUSE_TICKLESS_IDLE           1
+    #define configEXPECTED_IDLE_TIME_BEFORE_SLEEP   100
+    #define configPRE_SLEEP_PROCESSING( x ) xTaskSleepPrivate ( &x )
+    #define configPOST_SLEEP_PROCESSING( x ) xTaskWakeUpPrivate ( x )
+#endif
