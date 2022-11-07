1e33fce5e19c609ff6e20a82cfd7c7a65573ffbd
Marco Baldinetti <marcobaldinetti@gmail.com>
2022-11-07 12:47:39 +0100

    fix debug

--- platformio/stima_v4/master/src/debug.cpp
+++ platformio/stima_v4/master/src/debug.cpp
@@ -24,7 +24,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "debug.h"
 
 void init_debug(uint32_t baudrate) {
-  Serial4.begin(baudrate);
+  Serial.begin(baudrate);
 }
 
 /**
