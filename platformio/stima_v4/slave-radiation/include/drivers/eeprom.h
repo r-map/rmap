03541674a44ebd3a58b2f9915739fbf3ae87c31e
Moreno Gasperini <m.gasperini@digiteco.it>
2023-03-13 11:43:07 +0100

    Slave: Radiation
    Duplication model Radiation (first_commit). Work in progress

---
+++ platformio/stima_v4/slave-radiation/include/drivers/eeprom.h
@@ -0,0 +1,66 @@
+/**
+  ******************************************************************************
+  * @file    Eeprom.cpp
+  * @author  Moreno Gasperini <m.gasperini@digiteco.it>
+  * @brief   eeprom AT24C64 header file
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (C) 2022  Moreno Gasperini <m.gasperini@digiteco.it>
+  * All rights reserved.</center></h2>
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU General Public License
+  * as published by the Free Software Foundation; either version 2
+  * of the License, or (at your option) any later version.
+  * 
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  * 
+  * You should have received a copy of the GNU General Public License
+  * along with this program; if not, write to the Free Software
+  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+  * <http://www.gnu.org/licenses/>.
+  * 
+  ******************************************************************************
+*/
+
+
+#ifndef _EEPROM_H
+#define _EEPROM_H
+
+#include <STM32FreeRTOS.h>
+#include "ticks.hpp"
+#include "thread.hpp"
+#include "semaphore.hpp"
+#include "Wire.h"
+
+using namespace cpp_freertos;
+
+#define EEPROM_AT24C64_DEFAULT_ADDRESS (0x50)
+#define	EEPROMSIZE  8192
+#define EEPAGESIZE  32
+#define PAGEMASK    (EEPROMSIZE-EEPAGESIZE)
+#define WR_TIME_MS  5
+#define EEPROM_SEMAPHORE_MAX_WAITING_TIME_MS  (1000)
+
+class EEprom {
+
+public:
+  EEprom();
+  EEprom(TwoWire *wire, BinarySemaphore *wireLock, uint8_t i2c_address = EEPROM_AT24C64_DEFAULT_ADDRESS);
+  bool Write(uint16_t address, uint8_t *buffer, uint16_t length);
+  bool Write(uint16_t address, uint8_t value);
+  bool Read(uint16_t address, uint8_t *buffer, uint16_t length);
+  bool Read(uint16_t address, uint8_t *value);
+
+protected:
+private:
+  TwoWire *_wire;
+  BinarySemaphore *_wireLock;
+  uint8_t _i2c_address;
+};
+
+#endif
---
+++ platformio/stima_v4/slave-radiation/include/tasks/solar_radiation_sensor_task.h
@@ -0,0 +1,106 @@
+/**@file solar_radiation_sensor_task.h */
+
+/*********************************************************************
+Copyright (C) 2022  Marco Baldinetti <marco.baldinetti@digiteco.it>
+authors:
+Marco Baldinetti <marco.baldinetti@digiteco.it>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+<http://www.gnu.org/licenses/>.
+**********************************************************************/
+
+#ifndef _SOLAR_RADIATION_SENSOR_TASK_H
+#define _SOLAR_RADIATION_SENSOR_TASK_H
+
+#include "debug_config.h"
+#include "local_typedef.h"
+#include "stima_utility.h"
+#include "str.h"
+
+#if (MODULE_TYPE == STIMA_MODULE_TYPE_SOLAR_RADIATION)
+
+#define SOLAR_RADIATION_TASK_POWER_ON_WAIT_DELAY_MS  (100)
+#define SOLAR_RADIATION_TASK_WAIT_DELAY_MS           (50)
+#define SOLAR_RADIATION_TASK_GENERIC_RETRY_DELAY_MS  (5000)
+#define SOLAR_RADIATION_TASK_GENERIC_RETRY           (3)
+#define SOLAR_RADIATION_TASK_LOW_POWER_ENABLED       (true)
+#define SOLAR_RADIATION_TASK_ERROR_FOR_POWER_OFF     (SENSORS_COUNT_MAX * 3 * 2)
+
+#define WAIT_QUEUE_REQUEST_ELABDATA_MS  (50)
+
+#include <STM32FreeRTOS.h>
+#include "thread.hpp"
+#include "ticks.hpp"
+#include "semaphore.hpp"
+#include "queue.hpp"
+#include "drivers/module_slave_hal.hpp"
+
+#if (ENABLE_I2C1 || ENABLE_I2C2)
+#include <Wire.h>
+#endif
+
+// TODO:_TH_RAIN_
+// #include "SensorDriver.h"
+#include "debug_F.h"
+
+using namespace cpp_freertos;
+
+typedef struct {
+  configuration_t *configuration;
+  system_status_t *system_status;
+  cpp_freertos::BinarySemaphore *configurationLock;
+  cpp_freertos::BinarySemaphore *systemStatusLock;
+  cpp_freertos::Queue *systemMessageQueue;
+  cpp_freertos::Queue *elaborataDataQueue;
+} SolarRadiationSensorParam_t;
+
+class SolarRadiationSensorTask : public cpp_freertos::Thread {
+  typedef enum
+  {
+    SENSOR_STATE_CREATE,
+    SENSOR_STATE_WAIT_CFG,
+    SENSOR_STATE_INIT,
+    SENSOR_STATE_SETUP,
+    SENSOR_STATE_PREPARE,
+    SENSOR_STATE_READ,
+    SENSOR_STATE_END
+  } State_t;
+
+public:
+  SolarRadiationSensorTask(const char *taskName, uint16_t stackSize, uint8_t priority, SolarRadiationSensorParam_t solarRadiationSensorParam);
+
+protected:
+  virtual void Run();
+
+private:
+  #if (ENABLE_STACK_USAGE)
+  void TaskMonitorStack();
+  #endif
+  void TaskWatchDog(uint32_t millis_standby);
+  void TaskState(uint8_t state_position, uint8_t state_subposition, task_flag state_operation);
+
+  void powerOn();
+  void powerOff();
+
+  bool is_power_on;
+
+  State_t state;
+  SolarRadiationSensorParam_t param;
+  //TODO:_TH_RAIN_
+  //SensorDriver *sensors[SENSORS_COUNT_MAX];
+};
+
+#endif
+#endif
