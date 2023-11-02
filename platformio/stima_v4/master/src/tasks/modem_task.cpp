c905d4c36753cdcda38e6effedf67a38ab57d4f9
Paolo Patruno <p.patruno@iperbole.bologna.it>
2023-11-02 18:09:22 +0100

    strange u_int type

--- platformio/stima_v4/master/src/tasks/modem_task.cpp
+++ platformio/stima_v4/master/src/tasks/modem_task.cpp
@@ -51,7 +51,7 @@ ModemTask::ModemTask(const char *taskName, uint16_t stackSize, uint8_t priority,
 /// @brief local stack Monitor (optional)
 void ModemTask::TaskMonitorStack()
 {
-  u_int16_t stackUsage = (u_int16_t)uxTaskGetStackHighWaterMark( NULL );
+  uint16_t stackUsage = (uint16_t)uxTaskGetStackHighWaterMark( NULL );
   if((stackUsage) && (stackUsage < param.system_status->tasks[LOCAL_TASK_ID].stack)) {
     param.systemStatusLock->Take();
     param.system_status->tasks[LOCAL_TASK_ID].stack = stackUsage;
