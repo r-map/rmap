e091da8d20165ec74cd9b7fc29b96bfd8e530bc7
Paolo Patruno <p.patruno@iperbole.bologna.it>
2023-12-06 22:52:58 +0100

    reenable Brownout for esp32

--- platformio/stima_v2/stimawifi/src/stimawifi.ino
+++ platformio/stima_v2/stimawifi/src/stimawifi.ino
@@ -1317,9 +1317,9 @@ void logSuffix(Print* _logOutput) {
 void setup() {
   // put your setup code here, to run once:
 
-  #include "soc/soc.h"
-  #include "soc/rtc_cntl_reg.h"
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable Brownout detector
+  //#include "soc/soc.h"
+  //#include "soc/rtc_cntl_reg.h"
+  //WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable Brownout detector
   
   pinMode(RESET_PIN, INPUT_PULLUP);
   pinMode(LED_PIN, OUTPUT);
