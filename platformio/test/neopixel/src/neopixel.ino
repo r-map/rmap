aecadcc4a884aa117b1f6c6e8f83366bd56f6ade
Paolo Patruno <p.patruno@iperbole.bologna.it>
2024-01-24 14:06:09 +0100

    neopixel test

---
+++ platformio/test/neopixel/src/neopixel.ino
@@ -0,0 +1,26 @@
+#include <Adafruit_NeoPixel.h>
+ 
+#define PIN 47 // S3
+//#define PIN 7  // C3
+ 
+// Quando configuriamo la libreria NeoPixel, gli diciamo quanti pixel e quale pin usare per inviare segnali.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);
+ 
+void setup()
+{
+  pixels.begin(); // Si inizializza la libreria NeoPixel.
+}
+ 
+void loop()
+{
+ 
+  pixels.setPixelColor(0, pixels.Color(255, 0, 0)); //Colore rosso
+  pixels.show();
+  delay(1000);
+  pixels.setPixelColor(0, pixels.Color(0, 255, 0)); //Colore verde
+  pixels.show();
+  delay(1000);
+  pixels.setPixelColor(0, pixels.Color(0, 0, 255)); //Colore blu
+  pixels.show();
+  delay(1000);
+}
