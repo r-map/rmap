17bdb30d322ac3e71bbda1985f97586ecfd0799e
Paolo Patruno <p.patruno@iperbole.bologna.it>
2024-02-14 14:40:23 +0100

    Stima release 3.15

--- platformio/stima_v3/stima/src/stima-config.h
+++ platformio/stima_v3/stima/src/stima-config.h
@@ -39,7 +39,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 \def MODULE_MINOR_VERSION
 \brief Module minor version.
 */
-#define MODULE_MINOR_VERSION                          (14)
+#define MODULE_MINOR_VERSION                          (15)
 
 /*!
 \def MODULE_CONFIGURATION_VERSION
