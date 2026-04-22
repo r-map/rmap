1ab6231891129e33070234d96d3af3f8c35fdbd5
Paolo Patruno <p.patruno@iperbole.bologna.it>
2026-04-22 22:44:41 +0200

    stimawifi: distribute all bins

--- platformio/stima_v3/stimawifi/extra_script.py
+++ platformio/stima_v3/stimawifi/extra_script.py
@@ -2,14 +2,25 @@ from os.path import join
 Import("env", "projenv")
 
 # Dump global construction environment (for debug purpose)
-#print(env.Dump())
+print(env.Dump())
 
 # Custom BIN from ELF
 env.AddPostAction(
     "$BUILD_DIR/${PROGNAME}.bin",
-    env.VerboseAction(" ".join([
+    env.VerboseAction(" ".join(
+        [
         "mkdir", "-p", "$PROJECT_DIR/bin/${PIOENV};",
         "cp",
         "$BUILD_DIR/${PROGNAME}.bin",
-        "$PROJECT_DIR/bin/${PIOENV}/firmware.bin"
-    ]), "Building $TARGET"))
+        "$PROJECT_DIR/bin/${PIOENV}/firmware.bin;"
+        "cp",
+        "$BUILD_DIR/bootloader.bin",
+        "$PROJECT_DIR/bin/${PIOENV}/;"
+        "cp",
+        "$BUILD_DIR/partitions.bin",
+        "$PROJECT_DIR/bin/${PIOENV}/;"
+        "cp",
+        "$PROJECT_PACKAGES_DIR/framework-arduinoespressif32/tools/partitions/boot_app0.bin",
+        "$PROJECT_DIR/bin/${PIOENV}/"
+        ]
+    ), "Building $TARGET"))
